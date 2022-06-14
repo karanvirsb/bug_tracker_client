e00d912c758f21845a77d817328325caaca1a791
mrboparai <karanvirboparai123@gmail.com>
2022-06-14 09:35:22 -0400

    added on change check for username and password

--- src/Routes/Login/login.tsx
+++ src/Routes/Login/login.tsx
@@ -2,18 +2,40 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 type States = {
-    username: string;
-    password: string;
+    login: {
+        username: string;
+        password: string;
+    };
 };
 
 const Login = (): JSX.Element => {
     const navigate = useNavigate();
-    const [username, setUsername] = useState<States["username"]>("");
-    const [password, setPassword] = useState<States["password"]>("");
+
+    const [inputValues, setInputValues] = useState<States["login"]>({
+        username: "",
+        password: "",
+    });
+
+    const changedValue = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setInputValues({
+            ...inputValues,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (): void => {
+        if (!inputValues.username) throw Error("Invalid username");
+        if (!inputValues.password) throw Error("Invalid password");
+
+        // TODO submit the login data
+    };
 
     return (
         <section className='bg-main-color w-full min-h-screen flex justify-center items-center px-3 '>
-            <div className='bg-white p-4 flex justify-between items-center flex-col gap-4 w-80 sm:w-4/5 h-60 lg:h-80 xl:h-[20rem] rounded-md'>
+            <form
+                className='bg-white p-4 flex justify-between items-center flex-col gap-4 w-80 sm:w-4/5 h-60 lg:h-80 xl:h-[20rem] rounded-md'
+                onClick={handleSubmit}
+            >
                 <h1 className=' xl:text-4xl lg:text-3xl'>Login</h1>
                 <div className='h-full flex flex-col justify-evenly w-full'>
                     <input
@@ -21,12 +43,16 @@ const Login = (): JSX.Element => {
                         type='text'
                         name='username'
                         placeholder='Username'
+                        value={inputValues.username}
+                        onChange={changedValue}
                     />
                     <input
                         className='border-b-[2px] border-gray-400 w-full py-1 px-3 hover:outline-none focus:outline-none focus:border-secondary-color focus:border-b-[3px]  xl:text-2xl lg:text-xl placeholder:text-xl'
                         type='text'
                         name='password'
                         placeholder='Password'
+                        value={inputValues.password}
+                        onChange={changedValue}
                     />
                 </div>
                 <div className='flex flex-col justify-evenly w-full gap-4'>
@@ -42,7 +68,7 @@ const Login = (): JSX.Element => {
                         Register
                     </button>
                 </div>
-            </div>
+            </form>
         </section>
     );
 };
