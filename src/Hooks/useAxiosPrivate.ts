dc9247aa323c23cee0af496ee42a60dd1087a775
mrboparai <karanvirboparai123@gmail.com>
2022-06-18 10:05:48 -0400

    created hook that will add accessToken

---
+++ src/Hooks/useAxiosPrivate.ts
@@ -0,0 +1,53 @@
+import { axiosPrivate } from "../API/axios";
+import { useEffect } from "react";
+import useRefreshToken from "./useRefreshToken";
+import useAuth from "./useAuth";
+
+import { IStates } from "../Context/AuthProvider";
+
+const useAxiosPrivate = () => {
+    const refresh = useRefreshToken();
+    const { auth }: IStates = useAuth();
+
+    useEffect(() => {
+        const requestIntercept = axiosPrivate.interceptors.request.use(
+            (config: any) => {
+                if (!config.headers["Authorization"]) {
+                    config.headers[
+                        "Authorization"
+                    ] = `Bearer ${auth?.accessToken}`;
+                }
+                return config;
+            },
+            (error) => {
+                Promise.reject(error);
+            }
+        );
+
+        const responseIntercept = axiosPrivate.interceptors.response.use(
+            (response) => response,
+            async (error) => {
+                //token expired
+                const prevRequest = error?.config;
+                if (error?.response?.status === 403 && !prevRequest?.sent) {
+                    prevRequest.sent = true;
+                    const newAccessToken = await refresh();
+                    prevRequest.headers[
+                        "Authorization"
+                    ] = `Bearer ${newAccessToken}`;
+                    return axiosPrivate(prevRequest);
+                }
+                return Promise.reject(error);
+            }
+        );
+
+        return () => {
+            axiosPrivate.interceptors.response.eject(responseIntercept);
+            axiosPrivate.interceptors.request.eject(requestIntercept);
+        };
+    }, [auth, refresh]);
+
+    return axiosPrivate;
+};
+
+export default useAxiosPrivate;
