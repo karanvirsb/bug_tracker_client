16eee369a63bf52424930a59d7682df6f1c67888
mrboparai <karanvirboparai123@gmail.com>
2022-06-18 10:50:18 -0400

    added toast containter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 import { Login, Register } from "./Routes";
+import { ToastContainer } from "react-toastify";
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path='/login' element={<Login></Login>}></Route>
-                <Route path='/register' element={<Register></Register>}></Route>
-            </Routes>
-        </Router>
+        <>
+            <ToastContainer
+                position='top-right'
+                draggable={true}
+                draggablePercent={75}
+                autoClose={5000}
+            ></ToastContainer>
+            <Router>
+                <Routes>
+                    <Route path='/login' element={<Login></Login>}></Route>
+                    <Route
+                        path='/register'
+                        element={<Register></Register>}
+                    ></Route>
+                </Routes>
+            </Router>
+        </>
     );
 }
 
