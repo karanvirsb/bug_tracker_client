66982f5339772508b58509dd4de5265f2e8a8bc0
mrboparai <karanvirboparai123@gmail.com>
2022-06-18 10:27:53 -0400

    created persistLogin component

---
+++ src/Components/PersistLogin/persistLogin.tsx
@@ -0,0 +1,37 @@
+import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import useRefreshToken from "../../Hooks/useRefreshToken";
+import useAuth from "../../Hooks/useAuth";
+import { IStates } from "../../Context/AuthProvider";
+
+const PersistLogin = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const refresh = useRefreshToken();
+    const { auth, persist }: IStates = useAuth();
+
+    useEffect(() => {
+        let isMounted = true;
+        const verifyRefreshToken = async () => {
+            try {
+                await refresh();
+            } catch (error) {
+            } finally {
+                isMounted && setIsLoading(false);
+            }
+        };
+
+        // we need to have access to the accessToken before we can verifyREfreshToken
+
+        !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
+
+        return () => {
+            isMounted = false;
+        };
+    }, []);
+
+    return (
+        <>{!persist ? <Outlet></Outlet> : isLoading && <p>Loading....</p>}</>
+    );
+};
+
+export default PersistLogin;
