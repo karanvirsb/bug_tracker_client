243513f43e372920034f5c977c9e3cdff48ade13
mrboparai <karanvirboparai123@gmail.com>
2022-06-19 12:52:53 -0400

    added home route

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,4 +1,5 @@
 import Login from "./Login/login";
 import Register from "./Register/register";
+import Home from "./UserHome/home";
 
-export { Login, Register };
+export { Login, Register, Home };
