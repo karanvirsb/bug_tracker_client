b12d26198c9f81192234a50c5706ddbac8e7bc55
mrboparai <karanvirboparai123@gmail.com>
2022-06-19 12:52:31 -0400

    added protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,44 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
-import { Login, Register } from "./Routes";
+import { Home, Login, Register } from "./Routes";
 import { ToastContainer } from "react-toastify";
+import PersistLogin from "./Components/PersistLogin/persistLogin";
+import RequireAuth from "./Components/RequireAuth/RequireAuth";
 
 function App() {
     return (
-        <>
+        <Router>
             <ToastContainer
                 position='top-right'
                 draggable={true}
                 draggablePercent={75}
                 autoClose={5000}
             ></ToastContainer>
-            <Router>
-                <Routes>
-                    <Route path='/login' element={<Login></Login>}></Route>
+            <Routes>
+                <Route path='/login' element={<Login></Login>}></Route>
+                <Route path='/register' element={<Register></Register>}></Route>
+                {/* PROTECT ROUTES */}
+                <Route element={<PersistLogin></PersistLogin>}>
                     <Route
-                        path='/register'
-                        element={<Register></Register>}
-                    ></Route>
-                </Routes>
-            </Router>
-        </>
+                        element={
+                            <RequireAuth allowedRoles={["2001"]}></RequireAuth>
+                        }
+                    >
+                        {/* TODO add routes */}
+                        <Route path='/' element={<Home></Home>}></Route>
+                    </Route>
+                    <Route
+                        element={
+                            <RequireAuth allowedRoles={[1990]}></RequireAuth>
+                        }
+                    >
+                        {/* TODO add routes */}
+                        <Route path='/admin'></Route>
+                    </Route>
+                </Route>
+            </Routes>
+        </Router>
     );
 }
 
