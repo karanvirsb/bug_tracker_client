f7be6d96ac14ec9d14a4cb2abd1fd50be87bc1c3
mrboparai <karanvirboparai123@gmail.com>
2022-06-30 10:44:20 -0400

    added unauthorized to routes

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -3,5 +3,13 @@ import Register from "./Register/register";
 import Home from "./UserHome/home";
 import RegistrationSuccessful from "./Register-Successful/registration-successful";
 import PageNotFound from "./PageNotFound/pageNotFound";
+import Unauthorized from "./Unauthorized/unauthorized";
 
-export { Login, Register, Home, RegistrationSuccessful, PageNotFound };
+export {
+    Login,
+    Register,
+    Home,
+    RegistrationSuccessful,
+    PageNotFound,
+    Unauthorized,
+};
