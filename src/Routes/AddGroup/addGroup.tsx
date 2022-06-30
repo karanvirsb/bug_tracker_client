dd7c5b38c7c2c59b2a6d9eaf9da3ae388a9a17d0
mrboparai <karanvirboparai123@gmail.com>
2022-06-30 14:35:32 -0400

    added state to hold the group

--- src/Routes/AddGroup/addGroup.tsx
+++ src/Routes/AddGroup/addGroup.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+
+type States = {
+    groupType: string;
+};
 
 const AddGroup = () => {
+    const [group, setGroup] = useState<States["groupType"]>("");
     return (
         <section className='bg-main-color flex justify-center items-center p-4 w-full h-screen'>
             <div className='bg-white p-4 rounded-md flex flex-col gap-3'>
@@ -11,6 +16,8 @@ const AddGroup = () => {
                     type='text'
                     className='input'
                     placeholder='Invite / Group name'
+                    onChange={(e) => setGroup(e.target.value)}
+                    value={group}
                 />
                 <button className='bg-blue-500 text-white font-semibold hover:outline-blue-500 hover:text-black hover:outline-2 hover:outline hover:bg-transparent rounded-md mt-2 py-2 px-4 md:text-lg xl:text-xl text-center'>
                     Join
