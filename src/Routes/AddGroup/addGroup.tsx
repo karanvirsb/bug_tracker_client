a84af1c34ef86b9f1541dc4ec5ea76574976db39
mrboparai <karanvirboparai123@gmail.com>
2022-06-30 14:39:53 -0400

    created onclick events for join and create group

--- src/Routes/AddGroup/addGroup.tsx
+++ src/Routes/AddGroup/addGroup.tsx
@@ -6,6 +6,10 @@ type States = {
 
 const AddGroup = () => {
     const [group, setGroup] = useState<States["groupType"]>("");
+
+    const joinGroup = (): void => {};
+    const createGroup = (): void => {};
+
     return (
         <section className='bg-main-color flex justify-center items-center p-4 w-full h-screen'>
             <div className='bg-white p-4 rounded-md flex flex-col gap-3'>
@@ -19,10 +23,16 @@ const AddGroup = () => {
                     onChange={(e) => setGroup(e.target.value)}
                     value={group}
                 />
-                <button className='bg-blue-500 text-white font-semibold hover:outline-blue-500 hover:text-black hover:outline-2 hover:outline hover:bg-transparent rounded-md mt-2 py-2 px-4 md:text-lg xl:text-xl text-center'>
+                <button
+                    className='bg-blue-500 text-white font-semibold hover:outline-blue-500 hover:text-black hover:outline-2 hover:outline hover:bg-transparent rounded-md mt-2 py-2 px-4 md:text-lg xl:text-xl text-center'
+                    onClick={joinGroup}
+                >
                     Join
                 </button>
-                <button className='bg-secondary-color text-white font-semibold hover:outline-secondary-color hover:outline-2 hover:outline hover:bg-transparent hover:text-black rounded-md py-2 px-4 md:text-lg xl:text-xl text-center'>
+                <button
+                    className='bg-secondary-color text-white font-semibold hover:outline-secondary-color hover:outline-2 hover:outline hover:bg-transparent hover:text-black rounded-md py-2 px-4 md:text-lg xl:text-xl text-center'
+                    onClick={createGroup}
+                >
                     Create
                 </button>
             </div>
