adb777de3cf76ada3f8c6a217c6567fc668b732d
mrboparai <karanvirboparai123@gmail.com>
2022-06-30 10:41:31 -0400

    updated path

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -2,6 +2,6 @@ import Login from "./Login/login";
 import Register from "./Register/register";
 import Home from "./UserHome/home";
 import RegistrationSuccessful from "./Register-Successful/registration-successful";
-import PageNotFound from "./pageNotFound/pageNotFound";
+import PageNotFound from "./PageNotFound/pageNotFound";
 
 export { Login, Register, Home, RegistrationSuccessful, PageNotFound };
