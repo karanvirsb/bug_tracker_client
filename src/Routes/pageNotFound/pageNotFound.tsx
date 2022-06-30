9ee7cc45e28567d8adc49d340900a1776e400111
mrboparai <karanvirboparai123@gmail.com>
2022-06-30 10:37:02 -0400

    created page not found template

---
+++ src/Routes/pageNotFound/pageNotFound.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PageNotFound = () => {
+    return (
+        <section>
+            <div>
+                <h1>404 Page Not Found</h1>
+                <p>The page you are trying to access does not exist.</p>
+                <button>Go Back</button>
+            </div>
+        </section>
+    );
+};
+
+export default PageNotFound;
