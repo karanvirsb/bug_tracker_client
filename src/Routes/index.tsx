5111a577941858c696f3e43e0c80a4e5f3b8202a
mrboparai <karanvirboparai123@gmail.com>
2022-06-30 10:37:11 -0400

    added page not found to routes index

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -2,5 +2,6 @@ import Login from "./Login/login";
 import Register from "./Register/register";
 import Home from "./UserHome/home";
 import RegistrationSuccessful from "./Register-Successful/registration-successful";
+import PageNotFound from "./pageNotFound/pageNotFound";
 
-export { Login, Register, Home, RegistrationSuccessful };
+export { Login, Register, Home, RegistrationSuccessful, PageNotFound };
