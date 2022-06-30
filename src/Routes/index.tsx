d5033eb843a717cb75c39c261a1545d0b73f822e
mrboparai <karanvirboparai123@gmail.com>
2022-06-30 10:01:25 -0400

    added register successful

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,5 +1,6 @@
 import Login from "./Login/login";
 import Register from "./Register/register";
 import Home from "./UserHome/home";
+import RegistrationSuccessful from "./Register-Successful/registration-successful";
 
-export { Login, Register, Home };
+export { Login, Register, Home, RegistrationSuccessful };
