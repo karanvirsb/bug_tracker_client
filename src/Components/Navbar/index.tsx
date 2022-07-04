416d7caf277cdbed8c52361967251147148cfa53
mrboparai <karanvirboparai123@gmail.com>
2022-07-04 17:25:36 -0400

    removed height

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -33,7 +33,7 @@ export const Navbar = () => {
 
 export const AdminNavbar = () => {
     return (
-        <nav className='bg-main-color text-white fixed top-2 left-2 bottom-2 rounded-2xl p-4 min-h-[98.5vh] w-[175px] flex flex-col justify-between'>
+        <nav className='bg-main-color text-white fixed top-2 left-2 bottom-2 rounded-2xl p-4 w-[175px] flex flex-col justify-between'>
             <div className='pb-4 '>
                 {/* TODO make drop down */}
                 <h1 className='text-center border-b-2 text-xl pb-1'>
