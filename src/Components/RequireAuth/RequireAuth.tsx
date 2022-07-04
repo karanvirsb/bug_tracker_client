108444fc067732838b2bd3fced19ebd82654943a
mrboparai <karanvirboparai123@gmail.com>
2022-07-04 13:56:14 -0400

    removed interface

--- src/Components/RequireAuth/RequireAuth.tsx
+++ src/Components/RequireAuth/RequireAuth.tsx
@@ -1,11 +1,10 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 import useAuth from "../../Hooks/useAuth";
-import { IStates } from "../../Context/AuthProvider";
 import decoder from "../../Helper/decodeToken";
 import { IDecode } from "../../Helper/decodeToken";
 
 const RequireAuth = ({ allowedRoles }: any) => {
-    const { auth }: IStates = useAuth();
+    const { auth } = useAuth();
     const location = useLocation();
 
     const decoded: IDecode | undefined = decoder(auth?.accessToken || "");
