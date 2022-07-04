4c920b863c402b1cdc8e0257c3ea9ea6b138aae8
mrboparai <karanvirboparai123@gmail.com>
2022-07-04 11:40:25 -0400

    added redux toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./Context/AuthProvider";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { store } from "./Store/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
@@ -11,10 +13,12 @@ const root = ReactDOM.createRoot(
 const queryClient = new QueryClient();
 root.render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <AuthProvider>
-                <App />
-            </AuthProvider>
-        </QueryClientProvider>
+        <Provider store={store}>
+            <QueryClientProvider client={queryClient}>
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
+            </QueryClientProvider>
+        </Provider>
     </React.StrictMode>
 );
