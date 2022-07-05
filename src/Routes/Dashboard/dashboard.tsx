4e5e6561e2ee250d16b39a942141baab9f08be52
mrboparai <karanvirboparai123@gmail.com>
2022-07-05 17:25:26 -0400

    added margins for different screen sizes

--- src/Routes/Dashboard/dashboard.tsx
+++ src/Routes/Dashboard/dashboard.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Dashboard = () => {
-    return <div>Dashboard</div>;
+    return (
+        <section className='ml-[193px] md:ml-[58px]'>
+            <h1>Dashboard</h1>
+            <div></div>
+            <div></div>
+        </section>
+    );
 };
 
 export default Dashboard;
