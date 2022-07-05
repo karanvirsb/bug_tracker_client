c90d1df533072f1d618112d206c10583a8101198
mrboparai <karanvirboparai123@gmail.com>
2022-07-05 12:52:55 -0400

    added method to get roles and display adminstration

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -8,7 +8,11 @@ export const Navbar = () => {
     const logout = useLogout();
 
     const auth = useAppSelector((state) => state.auth);
-
+    const getRoles = () => {
+        const roles = auth.roles;
+        const role: string = "1990";
+        return roles?.includes(role);
+    };
     const openModal = () => {
         setShowNavigation(true);
     };
@@ -84,6 +88,14 @@ export const Navbar = () => {
                         >
                             Tickets
                         </Link>
+                        {getRoles() && (
+                            <Link
+                                className='rounded-md py-2 px-4 hover:font-semibold hover:bg-gray-400 hover:text-black'
+                                to='/administration'
+                            >
+                                Administration
+                            </Link>
+                        )}
                     </div>
                 </div>
                 <button
@@ -94,45 +106,5 @@ export const Navbar = () => {
                 </button>
             </nav>
         </header>
-    );
-};
-
-export const AdminNavbar = () => {
-    const logout = useLogout();
-    return (
-        <nav className='bg-main-color text-white fixed top-2 left-2 bottom-2 rounded-2xl p-4 w-[175px] flex flex-col justify-between'>
-            <div className='pb-4 '>
-                {/* TODO make drop down */}
-                <h1 className='text-center border-b-2 text-xl pb-1'>
-                    Group name
-                </h1>
-                <div className=' flex flex-col gap-4 py-4 px-4 text-center'>
-                    <Link
-                        className='rounded-md py-2 px-4 hover:font-semibold hover:bg-gray-400 hover:text-black'
-                        to='/dashboard'
-                    >
-                        Dashboard
-                    </Link>
-                    <Link
-                        className='rounded-md py-2 px-4 hover:font-semibold hover:bg-gray-400 hover:text-black'
-                        to='/tickets'
-                    >
-                        Tickets
-                    </Link>
-                    <Link
-                        className='rounded-md py-2 px-4 hover:font-semibold hover:bg-gray-400 hover:text-black'
-                        to='/administration'
-                    >
-                        Administration
-                    </Link>
-                </div>
-            </div>
-            <button
-                className='bg-secondary-color text-black py-2 px-4 rounded-md hover:bg-transparent hover:text-white hover:font-semibold hover:outline hover:outline-secondary-color hover:outline-2'
-                onClick={logout}
-            >
-                Logout
-            </button>
-        </nav>
     );
 };
