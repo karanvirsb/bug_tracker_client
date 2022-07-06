a513532a1d06878b6dc311fdfcb50323cede9652
mrboparai <karanvirboparai123@gmail.com>
2022-07-05 22:37:31 -0400

    added projects component along with took out tr

--- src/Routes/Dashboard/dashboard.tsx
+++ src/Routes/Dashboard/dashboard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Projects from "./Components/Projects";
 
 const Dashboard = () => {
     return (
@@ -33,53 +34,12 @@ const Dashboard = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr className='border-gray-200 border-b-2'>
-                                <th
-                                    scope='row'
-                                    className='px-6 py-3 text-gray-800 font-semibold'
-                                >
-                                    Bug Tracker
-                                </th>
-                                <td className='truncate max-w-[15ch] px-6 py-3'>
-                                    Lorem ipsum dolor, sit amet consectetur
-                                    adipisicing elit. Reprehenderit iste beatae
-                                    recusandae et accusamus, rem iure, iusto
-                                    alias consectetur odit dicta repellat
-                                    corporis, tenetur quasi enim aspernatur
-                                    molestias impedit consequuntur! Obcaecati
-                                    maiores magnam odio voluptates, beatae
-                                    dolores nihil deserunt nesciunt cumque.
-                                    Voluptate cupiditate quo aspernatur iste
-                                    amet blanditiis ipsa architecto placeat,
-                                    beatae recusandae excepturi odit dolor
-                                    exercitationem ipsam, quisquam accusantium.
-                                </td>
-                                <td className='px-6 py-3'>2022-01-20</td>
-                                <td className='px-6 py-3'>
-                                    Marin Kar, Lar Far
-                                </td>
-                                <td className='px-6 py-3'>
-                                    <svg
-                                        xmlns='http://www.w3.org/2000/svg'
-                                        className='h-6 w-6'
-                                        fill='none'
-                                        viewBox='0 0 24 24'
-                                        stroke='currentColor'
-                                        strokeWidth={2}
-                                    >
-                                        <path
-                                            strokeLinecap='round'
-                                            strokeLinejoin='round'
-                                            d='M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z'
-                                        />
-                                    </svg>
-                                </td>
-                            </tr>
+                            <Projects></Projects>
                         </tbody>
                     </table>
                     <div className='w-full flex justify-center items-center py-4 gap-4'>
                         <button className='pagination-btn'>Prev</button>
-                        <span className='bg-secondary-color w-10 h-10 text-2xl flex justify-center items-center text-center rounded-full'>
+                        <span className='bg-secondary-color text-white w-10 h-10 text-2xl flex justify-center items-center text-center rounded-full'>
                             1
                         </span>
                         <button className='bg-btn-color text-white py-1 px-2 rounded-md hover:bg-transparent hover:text-black hover:font-semibold hover:outline hover:outline-btn-color hover:outline-2'>
@@ -88,7 +48,11 @@ const Dashboard = () => {
                     </div>
                 </div>
             </div>
-            <div></div>
+            <div>
+                <div>
+                    <h2>Ticket Status</h2>
+                </div>
+            </div>
         </section>
     );
 };
