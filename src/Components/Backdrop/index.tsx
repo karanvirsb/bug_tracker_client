f44b5c40b57acfe408451b3593c560d7e0e61c1c
mrboparai <karanvirboparai123@gmail.com>
2022-07-07 13:55:18 -0400

    created backdrop for modals

---
+++ src/Components/Backdrop/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const Backdrop = ({ children }: any) => {
+    const backdropConstraints = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 },
+        exit: { opacity: 0 },
+    };
+
+    return (
+        <motion.div
+            variants={backdropConstraints}
+            initial='hidden'
+            animate='visible'
+            exit={"exit"}
+        >
+            {children}
+        </motion.div>
+    );
+};
+
+export default Backdrop;
