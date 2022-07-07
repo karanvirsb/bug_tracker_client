198ea9cbf425fc14bf32b6f3c2f80812ca7bd2a5
mrboparai <karanvirboparai123@gmail.com>
2022-07-07 13:38:50 -0400

    updated to use the new updateAccessToken function

--- src/Hooks/useRefreshToken.ts
+++ src/Hooks/useRefreshToken.ts
@@ -1,5 +1,5 @@
 import axios from "../API/axios";
-import { setAuth } from "../Auth/authenticationSlice";
+import { updateAccessToken } from "../Auth/authenticationSlice";
 import { useAppSelector, useAppDispatch } from "../Hooks/hooks";
 const useRefreshToken = () => {
     const auth = useAppSelector((state) => state.auth);
@@ -12,13 +12,7 @@ const useRefreshToken = () => {
             withCredentials: true,
         });
 
-        const newAuth = {
-            ...auth,
-            accessToken: response.data.accessToken,
-            roles: response.data.roles,
-        };
-        console.log(newAuth);
-        dispatch(setAuth(newAuth));
+        dispatch(updateAccessToken(response.data.accessToken));
 
         return response.data.accessToken; // allwos us to request again
     };
