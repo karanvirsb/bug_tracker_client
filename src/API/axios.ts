97e5069e16d443cc8462c9772d27a93bec6d23e9
mrboparai <karanvirboparai123@gmail.com>
2022-07-09 11:04:24 -0400

    added authorization header

--- src/API/axios.ts
+++ src/API/axios.ts
@@ -8,6 +8,6 @@ export default axios.create({
 
 export const axiosPrivate = axios.create({
     baseURL: BASE_URL,
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json", Authorization: "" },
     withCredentials: true,
 });
