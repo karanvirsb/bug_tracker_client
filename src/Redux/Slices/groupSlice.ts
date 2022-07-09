7f0047cc85f64231a5da74a64898bf5d21e37ce5
mrboparai <karanvirboparai123@gmail.com>
2022-07-09 11:37:27 -0400

    created groupState

--- src/Redux/Slices/groupSlice.ts
+++ src/Redux/Slices/groupSlice.ts
@@ -1,1 +1,40 @@
-export {};
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+export interface users {
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    roles: { [key: string | number]: string };
+}
+export interface GroupState {
+    groupId: string;
+    groupName: string;
+    groupInviteCode: string;
+    users: users[];
+}
+
+const initialState: GroupState = {
+    groupId: "",
+    groupName: "",
+    groupInviteCode: "",
+    users: [],
+};
+
+export const groupSlice = createSlice({
+    name: "group",
+    initialState: initialState,
+    reducers: {
+        setGroup: (state, action: PayloadAction<GroupState>) => {
+            return { ...state, ...action.payload };
+        },
+        setUsers: (state, action: PayloadAction<GroupState["users"]>) => {
+            return { ...state, ...action.payload };
+        },
+    },
+});
+
+export const { setGroup, setUsers } = groupSlice.actions;
+
+export default groupSlice.reducer;
