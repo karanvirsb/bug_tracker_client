5e6fcebbb6436118a14d14ccdedb3df1492b26ad
mrboparai <karanvirboparai123@gmail.com>
2022-07-09 15:14:11 -0400

    added useRef to track the div

--- src/Routes/Dashboard/Components/ProjectOptions.tsx
+++ src/Routes/Dashboard/Components/ProjectOptions.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useDispatch } from "react-redux";
 import { setModal } from "../../../Redux/Slices/modalSlice";
 
 type props = {
     projectId: string;
     setProjectOpen: any;
+    refs: typeof useRef;
 };
 
-const ProjectOptions = ({ projectId, setProjectOpen }: props) => {
+const ProjectOptions = ({ projectId, setProjectOpen, refs }: props) => {
     // on click dispatch message for modal
     const dispatch = useDispatch();
 
@@ -25,7 +26,10 @@ const ProjectOptions = ({ projectId, setProjectOpen }: props) => {
     const openDeleteModal = () => {};
 
     return (
-        <div className='absolute right-[100%] top-[50%] translate-y-[-50%] bg-white rounded-md z-40 outline outline-2 outline-gray-400 flex flex-col gap-2 p-1'>
+        <div
+            className='absolute right-[100%] top-[50%] translate-y-[-50%] bg-white rounded-md z-40 outline outline-2 outline-gray-400 flex flex-col gap-2 p-1'
+            ref={refs}
+        >
             <button className='btn bg-blue-400' onClick={openEditModal}>
                 Edit
             </button>
