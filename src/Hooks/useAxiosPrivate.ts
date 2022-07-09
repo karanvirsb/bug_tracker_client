c518c30156df0be5453539fa6ea12d3bee7759c5
mrboparai <karanvirboparai123@gmail.com>
2022-07-09 11:04:34 -0400

    removed useEffect

--- src/Hooks/useAxiosPrivate.ts
+++ src/Hooks/useAxiosPrivate.ts
@@ -8,44 +8,43 @@ const useAxiosPrivate = () => {
     const refresh = useRefreshToken();
     const auth = useAppSelector((state) => state.auth);
 
-    useEffect(() => {
-        const requestIntercept = axiosPrivate.interceptors.request.use(
-            (config: AxiosRequestConfig): AxiosRequestConfig => {
-                if (config.headers) {
-                    config.withCredentials = true;
-                    config.headers[
-                        "Authorization"
-                    ] = `Bearer ${auth.accessToken}`;
-                }
-                return config;
-            },
-            (error) => {
-                return Promise.reject(error);
+    // useEffect(() => {
+    const requestIntercept = axiosPrivate.interceptors.request.use(
+        (config: AxiosRequestConfig): Promise<AxiosRequestConfig> => {
+            axiosPrivate.defaults.headers.common["Authorization"] = "";
+            if (config.headers) {
+                config.headers["Authorization"] = `Bearer ${auth.accessToken}`;
             }
-        );
+            return Promise.resolve(config);
+        },
+        (error) => {
+            return Promise.reject(error);
+        }
+    );
 
-        const responseIntercept = axiosPrivate.interceptors.response.use(
-            (response: AxiosResponse) => response,
-            async (error) => {
-                //token expired
-                const prevRequest = error?.config;
-                if (error?.response?.status === 403 && !prevRequest?.sent) {
-                    prevRequest.sent = true;
-                    const newAccessToken = await refresh();
-                    prevRequest.headers[
-                        "Authorization"
-                    ] = `Bearer ${newAccessToken}`;
-                    return axiosPrivate(prevRequest);
-                }
-                return Promise.reject(error);
+    const responseIntercept = axiosPrivate.interceptors.response.use(
+        (response: AxiosResponse) => response,
+        async (error) => {
+            axiosPrivate.defaults.headers.common["Authorization"] = "";
+            //token expired
+            const prevRequest = error?.config;
+            if (error?.response?.status === 403 && !prevRequest?.sent) {
+                prevRequest.sent = true;
+                const newAccessToken = await refresh();
+                prevRequest.headers[
+                    "Authorization"
+                ] = `Bearer ${newAccessToken}`;
+                return axiosPrivate(prevRequest);
             }
-        );
+            return Promise.reject(error);
+        }
+    );
 
-        return () => {
-            axiosPrivate.interceptors.response.eject(responseIntercept);
-            axiosPrivate.interceptors.request.eject(requestIntercept);
-        };
-    }, [auth, refresh]);
+    //     return () => {
+    //         axiosPrivate.interceptors.response.eject(responseIntercept);
+    //         axiosPrivate.interceptors.request.eject(requestIntercept);
+    //     };
+    // }, [auth.accessToken, refresh]);
 
     return axiosPrivate;
 };
