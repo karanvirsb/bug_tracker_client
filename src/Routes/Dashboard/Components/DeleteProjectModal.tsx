89a4b5d9f00442b690a0bdeb1143100e172fda57
mrboparai <karanvirboparai123@gmail.com>
2022-07-10 10:47:42 -0400

    added gap between buttons and text

--- src/Routes/Dashboard/Components/DeleteProjectModal.tsx
+++ src/Routes/Dashboard/Components/DeleteProjectModal.tsx
@@ -16,7 +16,7 @@ const DeleteProjectModal = ({ projectId }: props) => {
         <motion.div className='flex justify-center items-center w-full h-full'>
             <form
                 action=''
-                className='bg-white flex flex-col justify-center items-center p-4 rounded-md w-1/4 max-w-[350px] h-1/4 min-h-[200px] max-h-[250px]'
+                className='bg-white flex flex-col justify-center items-center gap-4 p-4 rounded-md w-1/4 max-w-[350px] h-1/4 min-h-[200px] max-h-[250px]'
             >
                 <p className='text-center lg:text-xl md:text-lg'>
                     Are you sure you want to delete this project?
