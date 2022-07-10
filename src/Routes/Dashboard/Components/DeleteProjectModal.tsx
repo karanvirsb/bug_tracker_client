f940d360ab1c4c6e8727451e997cfe576a99d4c3
mrboparai <karanvirboparai123@gmail.com>
2022-07-10 10:36:15 -0400

    added closeModal functionality

--- src/Routes/Dashboard/Components/DeleteProjectModal.tsx
+++ src/Routes/Dashboard/Components/DeleteProjectModal.tsx
@@ -1,14 +1,21 @@
 import { motion } from "framer-motion";
+import { useDispatch } from "react-redux";
+import { resetModal } from "../../../Redux/Slices/modalSlice";
 
 type props = {
     projectId: string;
 };
 const DeleteProjectModal = ({ projectId }: props) => {
+    const dispatch = useDispatch();
+
+    const closeDeleteModal = () => {
+        dispatch(resetModal());
+    };
     return (
         <motion.div>
             <p>Are you sure you want to delete this project?</p>
             <button>Yes</button>
-            <button>No</button>
+            <button onClick={closeDeleteModal}>No</button>
         </motion.div>
     );
 };
