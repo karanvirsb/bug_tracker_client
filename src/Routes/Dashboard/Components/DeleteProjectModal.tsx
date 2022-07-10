b9363d029f77b048a9ff6700d1e5dcf13e621c2f
mrboparai <karanvirboparai123@gmail.com>
2022-07-10 10:44:15 -0400

    centered everything within the form

--- src/Routes/Dashboard/Components/DeleteProjectModal.tsx
+++ src/Routes/Dashboard/Components/DeleteProjectModal.tsx
@@ -16,9 +16,11 @@ const DeleteProjectModal = ({ projectId }: props) => {
         <motion.div className='flex justify-center items-center w-full h-full'>
             <form
                 action=''
-                className='bg-white p-4 rounded-md w-1/4 max-w-[350px] h-1/4 min-h-[200px] max-h-[250px]'
+                className='bg-white flex flex-col justify-center items-center p-4 rounded-md w-1/4 max-w-[350px] h-1/4 min-h-[200px] max-h-[250px]'
             >
-                <p>Are you sure you want to delete this project?</p>
+                <p className='text-center'>
+                    Are you sure you want to delete this project?
+                </p>
                 <button>Yes</button>
                 <button onClick={closeDeleteModal}>No</button>
             </form>
