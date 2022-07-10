44f34a8f9f26d6915bf22ccb819dd86fc3145603
mrboparai <karanvirboparai123@gmail.com>
2022-07-10 10:42:41 -0400

    added width and height to form

--- src/Routes/Dashboard/Components/DeleteProjectModal.tsx
+++ src/Routes/Dashboard/Components/DeleteProjectModal.tsx
@@ -14,7 +14,10 @@ const DeleteProjectModal = ({ projectId }: props) => {
 
     return (
         <motion.div className='flex justify-center items-center w-full h-full'>
-            <form action='' className=''>
+            <form
+                action=''
+                className='bg-white p-4 rounded-md w-1/4 max-w-[350px] h-1/4 min-h-[200px] max-h-[250px]'
+            >
                 <p>Are you sure you want to delete this project?</p>
                 <button>Yes</button>
                 <button onClick={closeDeleteModal}>No</button>
