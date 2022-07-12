f24797a088607bcc0312fcf1a432135be0e50649
mrboparai <karanvirboparai123@gmail.com>
2022-07-12 13:38:00 -0400

    added axios interceptor

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { store } from "./Redux/Store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { AxiosInterceptor } from "./Components/AxiosInterceptors";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
@@ -15,9 +16,11 @@ root.render(
     <React.StrictMode>
         <BrowserRouter>
             <Provider store={store}>
-                <QueryClientProvider client={queryClient}>
-                    <App />
-                </QueryClientProvider>
+                <AxiosInterceptor>
+                    <QueryClientProvider client={queryClient}>
+                        <App />
+                    </QueryClientProvider>
+                </AxiosInterceptor>
             </Provider>
         </BrowserRouter>
     </React.StrictMode>
