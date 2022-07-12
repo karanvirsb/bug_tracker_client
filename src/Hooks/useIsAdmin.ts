4ba5369cf84f8d498996efc31353c1d8001473bb
mrboparai <karanvirboparai123@gmail.com>
2022-07-12 13:39:44 -0400

    removed roles as a parameter

--- src/Hooks/useIsAdmin.ts
+++ src/Hooks/useIsAdmin.ts
@@ -1,12 +1,9 @@
 import { useCallback } from "react";
 import { useAppSelector } from "./hooks";
-type params = {
-    roles: string[] | undefined;
-};
+
 const useIsAdmin = () => {
     const roles = useAppSelector((state) => state.auth.roles);
     const getRoles = useCallback(() => {
-        console.log(roles);
         if (roles !== undefined) {
             const role: string = "1990";
             return roles.includes(role);
