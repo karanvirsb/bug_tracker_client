fd5cfac75fa271d933c43c3f240b2cbc2b3a4bed
mrboparai <karanvirboparai123@gmail.com>
2022-07-14 18:38:27 -0400

    created a component to display members

---
+++ src/Components/Members/index.tsx
@@ -0,0 +1,62 @@
+import React, { useMemo } from "react";
+import { useAppSelector } from "../../Hooks/hooks";
+
+type props = { usersArr: string[] };
+
+type user = {
+    username: String;
+    email: String;
+    firstName: String;
+    lastName: String;
+};
+
+type userElementProps = {
+    users: user[];
+};
+const Members = ({ usersArr }: props) => {
+    // const axiosPrivate = useAxiosPrivate();
+
+    const groupUsers = useAppSelector((state) => state.group.users);
+
+    const users = [];
+    for (let i = 0; i < groupUsers.length; i++) {
+        if (usersArr.includes(groupUsers[i].username)) {
+            users.push(groupUsers[i]);
+        }
+    }
+
+    const UserElements = ({ users }: userElementProps) => {
+        return (
+            <>
+                {useMemo(() => {
+                    return users?.map((user: user, index: number) => {
+                        if (index === users.length - 1) {
+                            return (
+                                <span key={user.username as string}>
+                                    {`${user.firstName} ${user.lastName}`}
+                                </span>
+                            );
+                        }
+                        return (
+                            <span key={user.username as string}>
+                                {`${user.firstName} ${user.lastName}, `}
+                            </span>
+                        );
+                    });
+                }, [users])}
+            </>
+        );
+    };
+
+    return (
+        <>
+            {users ? (
+                <UserElements users={users}></UserElements>
+            ) : (
+                <div>No Users</div>
+            )}
+        </>
+    );
+};
+
+export default Members;
