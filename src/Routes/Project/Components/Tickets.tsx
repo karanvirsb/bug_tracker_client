c6b09256324a2b88015c19efba9a51622b2e3fe8
mrboparai <karanvirboparai123@gmail.com>
2022-07-14 17:12:33 -0400

    wrapped tickets with React fragment

--- src/Routes/Project/Components/Tickets.tsx
+++ src/Routes/Project/Components/Tickets.tsx
@@ -23,20 +23,22 @@ const Tickets = ({ tickets }: props) => {
         <tr className='w-full text-center text-lg '>
             <td colSpan={1000}>
                 <p className='text-xl'>No results.</p>
-                <p>Maybe try to create a project or reload the page.</p>
+                <p>Maybe try to create a ticket or reload the page.</p>
             </td>
         </tr>
     ) : (
-        tickets.map((ticket) => {
-            const dateCreated = new Date(ticket.dateCreated);
-            return (
-                <Ticket
-                    key={ticket.ticketId}
-                    {...ticket}
-                    dateCreated={dateCreated}
-                ></Ticket>
-            );
-        })
+        <>
+            {tickets.map((ticket) => {
+                const dateCreated = new Date(ticket.dateCreated);
+                return (
+                    <Ticket
+                        key={ticket.ticketId}
+                        {...ticket}
+                        dateCreated={dateCreated}
+                    ></Ticket>
+                );
+            })}
+        </>
     );
 };
 
