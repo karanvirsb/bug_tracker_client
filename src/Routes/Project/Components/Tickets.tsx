dcb398a38f612d272b865fd59bfbbde9cc448daf
mrboparai <karanvirboparai123@gmail.com>
2022-07-14 11:50:13 -0400

    added interface and html

--- src/Routes/Project/Components/Tickets.tsx
+++ src/Routes/Project/Components/Tickets.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Ticket from "./Ticket";
+export interface ITicket {
+    ticketId: string;
+    dateCreated: Date;
+    title: string;
+    description: string;
+    assignedDev: string[];
+    time: number;
+    ticketStatus: string;
+    ticketSeverity: string;
+    ticketType: string;
+    reporterId: string;
+    projectId: string;
+}
 
-const Tickets = (tickets) => {
-    return <tr></tr>;
+export type props = {
+    tickets: ITicket[];
+};
+
+const Tickets = ({ tickets }: props) => {
+    return !tickets ? (
+        <tr className='w-full text-center text-lg '>
+            <td colSpan={1000}>
+                <p className='text-xl'>No results.</p>
+                <p>Maybe try to create a project or reload the page.</p>
+            </td>
+        </tr>
+    ) : (
+        tickets.map((ticket) => {
+            const dateCreated = new Date(ticket.dateCreated);
+            return <Ticket></Ticket>;
+        })
+    );
 };
 
 export default Tickets;
