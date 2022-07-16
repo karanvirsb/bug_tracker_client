67f95495e8d8f17dea6007e729fda29eed59eb99
mrboparai <karanvirboparai123@gmail.com>
2022-07-15 22:09:42 -0400

    updated with onClick event and transition

--- src/Components/Backdrop/index.tsx
+++ src/Components/Backdrop/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-const Backdrop = ({ children }: any) => {
+const Backdrop = ({ children, onClick }: any) => {
+    const transition = { duration: 0.4, ease: [0.43, 0.13, 0.23, 0.96] };
     const backdropConstraints = {
-        hidden: { opacity: 0 },
-        visible: { opacity: 1 },
-        exit: { opacity: 0 },
+        hidden: { opacity: 0, transition },
+        visible: { opacity: 1, transition },
+        exit: { opacity: 0, transition },
     };
 
     return (
@@ -14,7 +15,10 @@ const Backdrop = ({ children }: any) => {
             initial='hidden'
             animate='visible'
             exit={"exit"}
-            className={"bg-backdrop-bg fixed inset-0 z-50"}
+            className={
+                "bg-backdrop-bg fixed inset-0 flex justify-center items-center flex-col"
+            }
+            onClick={onClick}
         >
             {children}
         </motion.div>
