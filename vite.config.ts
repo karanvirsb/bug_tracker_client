3b284ff425f154b742ecbc76f4200b7c2e865310
mrboparai <karanvirboparai123@gmail.com>
2022-07-16 10:08:36 -0400

    added server port

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        port: 3000,
+    },
     // This changes the out put dir from dist to build
     build: {
         outDir: "build",
