7644ae9a7333c2cb01f35a339ab8b92ae2221a76
mrboparai <karanvirboparai123@gmail.com>
2022-07-16 10:09:01 -0400

    removed listeners

--- src/API/sockets.ts
+++ src/API/sockets.ts
@@ -1,16 +1,7 @@
 import io from "socket.io-client";
 
 const socket = io("http://localhost:8000", {
-    transports: ["websocket"],
     reconnectionAttempts: 5,
-});
-
-socket.on("connection", (socket) => {
-    console.log("connected: " + socket);
-});
-
-socket.on("disconnect", () => {
-    console.log("disconnected");
 });
 
 export default socket;
