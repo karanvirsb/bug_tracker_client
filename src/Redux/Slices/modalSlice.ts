a88e3af6571c1969082de41255cf2606510ceb01
mrboparai <karanvirboparai123@gmail.com>
2022-07-19 15:13:37 -0400

    added update and delete ticket type

--- src/Redux/Slices/modalSlice.ts
+++ src/Redux/Slices/modalSlice.ts
@@ -7,6 +7,8 @@ export interface ModalState {
         | "updateProject"
         | "deleteProject"
         | "createTicket"
+        | "updateTicket"
+        | "deleteTicket"
         | "";
     open: boolean;
     options: { projectId?: string; ticketId?: string };
