1c682e9029c243b276fe19f454853d8ef9f17ce7
mrboparai <karanvirboparai123@gmail.com>
2022-07-19 23:01:52 -0400

    added overflow

--- src/Routes/Dashboard/Components/EditProjectModal.tsx
+++ src/Routes/Dashboard/Components/EditProjectModal.tsx
@@ -143,7 +143,7 @@ const EditProjectModal = (props: { projectId: string }): JSX.Element => {
 
     return (
         <motion.div
-            className='bg-white min-h-[100vh] w-1/3 lg:w-3/6 md:w-3/4 sm:w-full fixed right-0'
+            className='bg-white min-h-[100vh] w-1/3 lg:w-3/6 md:w-3/4 sm:w-full fixed right-0 top-0 bottom-0 overflow-auto'
             variants={modalConstraints}
         >
             <form
