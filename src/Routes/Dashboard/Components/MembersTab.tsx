9f9f4c2c594545d3ed94921552b6729652580cde
mrboparai <karanvirboparai123@gmail.com>
2022-07-22 16:24:34 -0400

    created members head

--- src/Routes/Dashboard/Components/MembersTab.tsx
+++ src/Routes/Dashboard/Components/MembersTab.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const MembersTab = () => {
-    return <div>MembersTab</div>;
+    return (
+        <>
+            <h1 className='text-2xl font-semibold'>Members</h1>
+            <table className='w-full outline outline-[1px] outline-gray-300 rounded-md'>
+                <thead className='text-sm text-gray-500 font-normal'>
+                    <tr>
+                        <th scope='col' className='px-6 py-3 sm:hidden'>
+                            {/* Avatar */}
+                        </th>
+                        <th scope='col' className='px-6 py-3 md:hidden'>
+                            USERNAME
+                        </th>
+                        <th scope='col' className='px-6 py-3 sm:text-center'>
+                            FULL NAME
+                        </th>
+                        <th scope='col' className='px-6 py-3 md:hidden'>
+                            EMAIL
+                        </th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+        </>
+    );
 };
 
 export default MembersTab;
