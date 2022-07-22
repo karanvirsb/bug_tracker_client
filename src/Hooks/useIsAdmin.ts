0a9809b3ddc3097d046e805967f6636e16875970
mrboparai <karanvirboparai123@gmail.com>
2022-07-22 10:31:05 -0400

    added comment

--- src/Hooks/useIsAdmin.ts
+++ src/Hooks/useIsAdmin.ts
@@ -1,8 +1,10 @@
 import { useCallback } from "react";
 import { useAppSelector } from "./hooks";
 
+// Checking to see if a user is an admin or an editor
 const useIsAdmin = () => {
     const user = useAppSelector((state) => state.persistedReducer.user);
+
     const getRoles = useCallback(() => {
         return user.isAdmin || user.isEditor;
     }, [user.isAdmin, user.isEditor]);
