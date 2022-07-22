7872deb8429e4dffaf2b1e12695f553cca94f389
mrboparai <karanvirboparai123@gmail.com>
2022-07-22 17:15:09 -0400

    removed comments

--- src/Routes/Dashboard/Components/Project.tsx
+++ src/Routes/Dashboard/Components/Project.tsx
@@ -1,11 +1,4 @@
 import React from "react";
-// import { useQuery } from "react-query";
-// import Spinner from "../../../Components/Spinner";
-// import axiosPrivate from "../../../Components/AxiosInterceptors";
-// import useComponentVisible from "../../../Hooks/useComponentVisible";
-// import useIsAdmin from "../../../Hooks/useIsAdmin";
-// import ProjectOptions from "./ProjectOptions";
-// import { useAppSelector } from "../../../Hooks/hooks";
 import Members from "../../../Components/Members";
 
 type project = {
@@ -37,10 +30,6 @@ const Project = ({
     users,
     setSelectedId,
 }: project) => {
-    // const { ref, isComponentVisible, setIsComponentVisible } =
-    //     useComponentVisible(false);
-    // const { getRoles } = useIsAdmin();
-    // const navigate = useNavigate();
     return (
         <tr
             className='border-gray-200 border-b-2 hover:bg-gray-200 cursor-pointer'
@@ -62,33 +51,6 @@ const Project = ({
             <td className='px-6 py-3 sm:hidden'>
                 <Members usersArr={users}></Members>
             </td>
-            {/* only if user is admin should you display this column */}
-            {/* {getRoles() && (
-                <td className='px-1 py-3 relative'>
-                    <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        className='h-6 w-6 relative'
-                        fill='none'
-                        viewBox='0 0 24 24'
-                        stroke='currentColor'
-                        strokeWidth={2}
-                        onClick={() => setIsComponentVisible(true)}
-                    >
-                        <path
-                            strokeLinecap='round'
-                            strokeLinejoin='round'
-                            d='M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z'
-                        />
-                    </svg> */}
-            {/* {isComponentVisible && (
-                        <ProjectOptions
-                            projectId={projectId}
-                            refs={ref}
-                            setProjectOpen={setIsComponentVisible}
-                        ></ProjectOptions>
-                    )} */}
-            {/* </td> */}
-            {/* )} */}
         </tr>
     );
 };
