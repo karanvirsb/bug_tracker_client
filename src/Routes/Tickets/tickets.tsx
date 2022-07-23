d88c65efdaad102a04a18689587a5e6c08c1dcc2
mrboparai <karanvirboparai123@gmail.com>
2022-07-23 09:52:47 -0400

    added fetching tickets

--- src/Routes/Tickets/tickets.tsx
+++ src/Routes/Tickets/tickets.tsx
@@ -1,6 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import axiosPrivate from "../../Components/AxiosInterceptors";
+import { useAppSelector } from "../../Hooks/hooks";
+
+type fetchTicketsByUsernameType = {
+    username: string;
+    currentPage: number;
+};
 
 const Tickets = () => {
+    const [pageNumber, setPageNumber] = useState(1);
+    const username = useAppSelector(
+        (state) => state.persistedReducer.user.username
+    );
+
+    const fetchTicketsByUsername = async ({
+        username,
+        currentPage,
+    }: fetchTicketsByUsernameType) => {
+        const resp = await axiosPrivate("/tickets/user/" + username, {
+            method: "get",
+            params: { page: currentPage },
+        });
+        return resp.data;
+    };
+
+    const {
+        data: userTickets,
+        status: ticketStatus,
+        refetch,
+    } = useQuery(
+        "userTickets",
+        () =>
+            fetchTicketsByUsername({
+                username: username,
+                currentPage: pageNumber,
+            }),
+        {
+            keepPreviousData: true,
+        }
+    );
+
     return <div>Tickets</div>;
 };
 
