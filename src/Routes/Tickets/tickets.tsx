1e0b84da37b0628ba9c5d55eaa071f8cee3eb9d5
mrboparai <karanvirboparai123@gmail.com>
2022-07-23 09:56:55 -0400

    added userTickets body

--- src/Routes/Tickets/tickets.tsx
+++ src/Routes/Tickets/tickets.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useQuery } from "react-query";
 import axiosPrivate from "../../Components/AxiosInterceptors";
+import Pagination from "../../Components/Pagination";
+import Spinner from "../../Components/Spinner";
 import { useAppSelector } from "../../Hooks/hooks";
 
 type fetchTicketsByUsernameType = {
@@ -10,6 +12,7 @@ type fetchTicketsByUsernameType = {
 
 const Tickets = () => {
     const [pageNumber, setPageNumber] = useState(1);
+    const [errMsg, setErrMsg] = useState("");
     const username = useAppSelector(
         (state) => state.persistedReducer.user.username
     );
@@ -41,7 +44,81 @@ const Tickets = () => {
         }
     );
 
-    return <div>Tickets</div>;
+    return (
+        <section className='sections'>
+            <div className='my-6 mx-4'>
+                <h1 className='text-xl font-semibold text-gray-800'>
+                    My Tickets
+                </h1>
+                <div className='outline-[#D4D4D4] outline-1 outline w-full px-4 text-left rounded-md'>
+                    <table className=' w-full'>
+                        <thead className='text-sm text-gray-500 font-normal'>
+                            <tr>
+                                <th
+                                    scope='col'
+                                    className='px-6 py-3 sm:text-center'
+                                >
+                                    TITLE
+                                </th>
+                                <th scope='col' className='px-6 py-3 lg:hidden'>
+                                    REPORTER
+                                </th>
+                                <th scope='col' className='px-6 py-3 lg:hidden'>
+                                    DATE CREATED
+                                </th>
+                                <th scope='col' className='px-6 py-3 md:hidden'>
+                                    TYPE
+                                </th>
+                                <th scope='col' className='px-6 py-3 sm:hidden'>
+                                    STATUS
+                                </th>
+                                <th scope='col' className='px-6 py-3 lg:hidden'>
+                                    ASSIGNEE
+                                </th>
+                                <th scope='col' className='px-6 py-3 sm:hidden'>
+                                    SEVERITY
+                                </th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            {ticketStatus === "loading" && (
+                                <tr className='w-full text-center'>
+                                    <td colSpan={99}>
+                                        <Spinner></Spinner>
+                                    </td>
+                                </tr>
+                            )}
+                            {ticketStatus === "success" && (
+                                <UserTickets
+                                    tickets={userTickets.docs}
+                                ></UserTickets>
+                            )}
+                            {ticketStatus === "error" && (
+                                <tr className='w-full text-center text-lg '>
+                                    <td colSpan={1000}>
+                                        <p className='my-3 text-xl'>{errMsg}</p>
+                                        <button
+                                            className='btn bg-gray-300 mb-5'
+                                            onClick={() => refetch}
+                                        >
+                                            try again
+                                        </button>
+                                    </td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                    <Pagination
+                        pageNumber={pageNumber}
+                        totalPage={userTickets?.totalPage || 0}
+                        hasMore={userTickets?.hasNextPage || false}
+                        setPageNumber={setPageNumber}
+                    ></Pagination>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Tickets;
