c50f81ad681eb38857664b2b6a267c1d2a998ace
mrboparai <karanvirboparai123@gmail.com>
2022-07-25 16:23:52 -0400

    updated onSuccess of submit

--- src/Routes/Administration/Components/EditMemberModal.tsx
+++ src/Routes/Administration/Components/EditMemberModal.tsx
@@ -1,6 +1,9 @@
+import { AxiosError } from "axios";
 import { motion } from "framer-motion";
 import React, { useState } from "react";
 import { useMutation } from "react-query";
+import { toast } from "react-toastify";
+import socket from "../../../API/sockets";
 import axiosPrivate from "../../../Components/AxiosInterceptors";
 import { useAppDispatch, useAppSelector } from "../../../Hooks/hooks";
 import { resetModal } from "../../../Redux/Slices/modalSlice";
@@ -17,17 +20,15 @@ type editMemberMutationType = {
         username?: string;
         firstName?: string;
         lastName?: string;
-        avatar?: string;
+        avatar?: { data: any; contentType: string };
         email?: string;
-        roles: {};
+        roles?: { [key: string]: string };
     };
 };
 
 const EditMemberModal = ({ username }: props) => {
-    const groupUsers = useAppSelector(
-        (state) => state.persistedReducer.group.users
-    );
-    const user = groupUsers.find((user) => user.username === username);
+    const group = useAppSelector((state) => state.persistedReducer.group);
+    const user = group.users.find((user) => user.username === username);
 
     const [memberInput, setMemberInput] = useState<IUser>({
         username: user?.username ?? "",
@@ -67,6 +68,73 @@ const EditMemberModal = ({ username }: props) => {
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+
+        const roles: { [key: string]: string } = { User: "2001" };
+        if (memberInput.isAdmin === true) {
+            roles["Admin"] = "1990";
+        }
+
+        if (memberInput.isEditor === true) {
+            roles["Editor"] = "1991";
+        }
+
+        const updates = {
+            username: memberInput.username,
+            firstName: memberInput.firstName,
+            lastName: memberInput.lastName,
+            email: memberInput.email,
+            avatar: memberInput.avatar,
+            roles: roles,
+        };
+
+        editMemberMutation.mutateAsync(
+            { id: memberInput.username, updates: updates },
+            {
+                onSuccess: () => {
+                    setMemberInput({
+                        username: "",
+                        firstName: "",
+                        lastName: "",
+                        email: "",
+                        avatar: { data: "", contentType: "" },
+                        isAdmin: false,
+                        isEditor: false,
+                    });
+                    dispatch(resetModal());
+
+                    // TODO update only user that has been updated.
+                    socket.emit("invalidateQuery", {
+                        queryName: "groupUsers",
+                        groupId: group.groupId,
+                    });
+
+                    socket.emit("invalidateQuery", {
+                        queryName: "userLogin",
+                        groupId: group.groupId,
+                    });
+                },
+                onError: (error) => {
+                    if (error instanceof AxiosError) {
+                        const errorResp = JSON.parse(
+                            error.response?.data.message
+                        );
+                        errorResp.forEach(
+                            (elem: {
+                                code: string;
+                                inclusive: boolean;
+                                message: string;
+                                minimum?: number;
+                                maxiumum?: number;
+                                path: string[];
+                                type: string;
+                            }) => {
+                                toast.error(elem.path[0] + " " + elem.message);
+                            }
+                        );
+                    }
+                },
+            }
+        );
     };
 
     return (
