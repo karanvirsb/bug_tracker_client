068c8a2b12be3064a8201e4c165f1adf7dcd2566
mrboparai <karanvirboparai123@gmail.com>
2022-07-25 11:04:11 -0400

    made modal for members

---
+++ src/Routes/Administration/Components/MemberModal.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { IUser } from "../../../Redux/Slices/userSlice";
+
+type props = {
+    memberInput: IUser;
+    setMemberInput: React.Dispatch<React.SetStateAction<IUser>>;
+};
+
+const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
+
+const MemberModal = ({ memberInput, setMemberInput }: props) => {
+    return (
+        <>
+            <div className='input-container'>
+                <label htmlFor='username' className='input-label'>
+                    Username
+                </label>
+                <input
+                    type='text'
+                    name='username'
+                    id='username'
+                    className='modal-input'
+                    readOnly
+                />
+            </div>
+            <div className='input-container'>
+                <label htmlFor='firstName' className='input-label'>
+                    First Name
+                </label>
+                <input
+                    type='text'
+                    name='firstName'
+                    id='firstName'
+                    className='modal-input'
+                    readOnly
+                />
+            </div>
+            <div className='input-container'>
+                <label htmlFor='lastName' className='input-label'>
+                    Last Name
+                </label>
+                <input
+                    type='text'
+                    name='lastName'
+                    id='lastName'
+                    className='modal-input'
+                    readOnly
+                />
+            </div>
+            <div className='input-container'>
+                <label htmlFor='email' className='input-label'>
+                    Email
+                </label>
+                <input
+                    type='text'
+                    name='email'
+                    id='email'
+                    className='modal-input'
+                    readOnly
+                />
+            </div>
+            <fieldset className='input-container'>
+                <legend className='input-label'>Roles</legend>
+                <div>
+                    <input
+                        type='checkbox'
+                        name='isAdmin'
+                        id='admin'
+                        value={`admin`}
+                        className='modal-input'
+                    />
+                    <label htmlFor='admin'>Admin</label>
+                    <input
+                        type='checkbox'
+                        name='roles'
+                        id='editor'
+                        value='editor'
+                        className='modal-input'
+                    />
+                    <label htmlFor='editor'>Editor</label>
+                </div>
+            </fieldset>
+        </>
+    );
+};
+
+export default MemberModal;
