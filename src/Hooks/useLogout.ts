dd36717a824b7f75ac203cdedba8608150190cd8
mrboparai <karanvirboparai123@gmail.com>
2022-07-27 11:47:42 -0400

    reset most of the state

--- src/Hooks/useLogout.ts
+++ src/Hooks/useLogout.ts
@@ -2,15 +2,39 @@ import axios from "../API/axios";
 import { setAuth } from "../Auth/authenticationSlice";
 import { useDispatch } from "react-redux";
 import socket from "../API/sockets";
+import { setGroup } from "../Redux/Slices/groupSlice";
+import { setUser } from "../Redux/Slices/userSlice";
 
 const useLogout = () => {
     const dispatch = useDispatch();
     const logout = async () => {
+        // reset auth
+
         dispatch(
-            setAuth({ username: "", accessToken: "", group_id: "", roles: [] })
-        ); // reset auth
+            setGroup({
+                groupId: "",
+                groupInviteCode: "",
+                groupName: "",
+                users: [],
+            })
+        );
+        dispatch(
+            setUser({
+                avatar: { data: "", contentType: "" },
+                username: "",
+                email: "",
+                firstName: "",
+                lastName: "",
+                isAdmin: false,
+                isEditor: false,
+            })
+        );
+
         socket.emit("leavingPage");
         socket.disconnect();
+        dispatch(
+            setAuth({ username: "", accessToken: "", group_id: "", roles: [] })
+        );
         try {
             await axios.delete("/logout", { withCredentials: true });
         } catch (err) {
