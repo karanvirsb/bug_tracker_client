eee274da4d07e0e4e6aa77076150c3b2d2989618
mrboparai <karanvirboparai123@gmail.com>
2022-07-27 14:41:24 -0400

    added a join room on dashboard

--- src/Routes/Dashboard/dashboard.tsx
+++ src/Routes/Dashboard/dashboard.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAppSelector } from "../../Hooks/hooks";
 
 import DashboardTab from "./Components/DashboardTab";
 import Tab from "../../Components/Tab/Tab";
 import MembersTab from "../../Components/MembersTab/MembersTab";
+import socket from "../../API/sockets";
 
 const Dashboard = () => {
+    const auth = useAppSelector((state) => state.persistedReducer.auth);
     const groupUsers = useAppSelector(
         (state) => state.persistedReducer.group.users
     );
@@ -29,6 +31,21 @@ const Dashboard = () => {
             label: "Members",
         },
     ];
+
+    useEffect(() => {
+        socket.emit("joinRoom", {
+            roomId: auth?.group_id,
+            username: auth.username,
+        });
+
+        socket.on("roomJoined", (join) => {
+            console.log("room join: " + join);
+        });
+
+        return () => {
+            socket.off("roomJoined");
+        };
+    }, []);
 
     return (
         <section className='sections'>
