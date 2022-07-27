80867fdb879d47bb7a28041d1ffd727dc73254fc
mrboparai <karanvirboparai123@gmail.com>
2022-07-27 14:27:27 -0400

    removed rendering users

--- src/Routes/Dashboard/dashboard.tsx
+++ src/Routes/Dashboard/dashboard.tsx
@@ -1,37 +1,17 @@
-import React, { useEffect } from "react";
-import { useAppDispatch, useAppSelector } from "../../Hooks/hooks";
-
-import { useQuery } from "react-query";
-import Spinner from "../../Components/Spinner";
-import axiosPrivate from "../../Components/AxiosInterceptors";
-import { setUsers } from "../../Redux/Slices/groupSlice";
+import React from "react";
+import { useAppSelector } from "../../Hooks/hooks";
 
 import DashboardTab from "./Components/DashboardTab";
 import Tab from "../../Components/Tab/Tab";
 import MembersTab from "../../Components/MembersTab/MembersTab";
 
 const Dashboard = () => {
-    const dispatch = useAppDispatch();
     const groupUsers = useAppSelector(
         (state) => state.persistedReducer.group.users
     );
     // getting the group Id
     const groupId = useAppSelector(
         (state) => state.persistedReducer.auth.group_id
-    );
-
-    // fetching all the users of the group
-    const fetchGroupUsers = async () => {
-        const resp = await axiosPrivate("/user/group", {
-            method: "Post",
-            data: { groupId: groupId },
-        });
-        return resp.data;
-    };
-
-    const { data: usersData, status: groupUsersStatus } = useQuery(
-        "groupUsers",
-        fetchGroupUsers
     );
 
     const components = {
@@ -50,26 +30,6 @@ const Dashboard = () => {
         },
     ];
 
-    useEffect(() => {
-        const newUsers = usersData?.map((user: any) => {
-            const isAdmin = Object.values(user.roles).includes("1990");
-            const isEditor = Object.values(user.roles).includes("1991");
-
-            return {
-                avatar: user.avatar,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: user.email,
-                username: user.username,
-                isAdmin,
-                isEditor,
-            };
-        });
-
-        if (groupUsersStatus === "success") {
-            dispatch(setUsers(newUsers));
-        }
-    }, [groupUsersStatus, usersData, dispatch]);
     return (
         <section className='sections'>
             <Tab tabs={tabs} components={components}></Tab>
