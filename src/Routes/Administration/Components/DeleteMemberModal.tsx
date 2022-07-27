42f695cd7a69d43b928599d4451e4e474b59c3a9
mrboparai <karanvirboparai123@gmail.com>
2022-07-27 09:47:49 -0400

    created the delete member modal

---
+++ src/Routes/Administration/Components/DeleteMemberModal.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { useMutation } from "react-query";
+import { useAppDispatch } from "../../../Hooks/hooks";
+import { resetModal } from "../../../Redux/Slices/modalSlice";
+import axiosPrivate from "../../../Components/AxiosInterceptors";
+import { toast } from "react-toastify";
+import socket from "../../../API/sockets";
+
+type props = {
+    username: string;
+};
+
+type deleteMemberMutationType = {
+    id: string;
+    updates: Object;
+};
+
+const DeleteMemberModal = ({ username }: props) => {
+    const dispatch = useAppDispatch();
+
+    const transition = { duration: 0.4, ease: [0.43, 0.13, 0.23, 0.96] };
+
+    const modalConstraints = {
+        hidden: { opacity: 0, y: 100, transition },
+        visible: { opacity: 1, y: 0, transition },
+        exit: {
+            scale: 0.2,
+            transition: { duration: 0.75, ease: [0.43, 0.13, 0.23, 0.96] },
+        },
+    };
+
+    const deleteMemberMutation = useMutation(
+        async ({ id, updates }: deleteMemberMutationType) => {
+            const resp = await axiosPrivate("/user/id", {
+                method: "put",
+                data: { id: id, updates: updates },
+            });
+            return resp.data;
+        }
+    );
+
+    const closeDeleteModal = () => {
+        dispatch(resetModal());
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        deleteMemberMutation.mutateAsync(
+            { id: username, updates: { groupId: "" } },
+            {
+                onSuccess: () => {
+                    socket.emit("removedUserFromGroup", { username: username });
+
+                    toast.success(`Successfully removed @${username}`);
+
+                    dispatch(resetModal());
+                },
+            }
+        );
+    };
+
+    return (
+        <motion.div
+            className='flex justify-center items-center w-full h-full'
+            variants={modalConstraints}
+            initial='hidden'
+            animate='visible'
+            exit='exit'
+        >
+            <form
+                action=''
+                className='bg-white flex flex-col justify-center items-center gap-4 p-4 rounded-md w-1/4 max-w-[350px] min-w-[250px] sm:w-[90%] h-1/4 min-h-[200px] max-h-[250px]'
+                onSubmit={handleSubmit}
+            >
+                <p className='text-center lg:text-xl m-md:text-lg'>
+                    {`Are you sure you want to kick ${username} user?`}
+                </p>
+                <div className='flex gap-2 sm:w-full sm:flex-col sm:items-stretch sm:px-16'>
+                    <button className='btn bg-blue-400 !px-6' type='submit'>
+                        Yes
+                    </button>
+                    <button
+                        className='btn bg-red-400 !px-6'
+                        onClick={closeDeleteModal}
+                        type='button'
+                    >
+                        No
+                    </button>
+                </div>
+            </form>
+        </motion.div>
+    );
+};
+
+export default DeleteMemberModal;
