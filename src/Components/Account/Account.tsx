72116dee8161364a3f7dec97a56dcf4eb3e87c5e
karanvirsb <karanvirboparai123@gmail.com>
2022-07-30 10:27:18 -0400

    added opening and closing of the menu

--- src/Components/Account/Account.tsx
+++ src/Components/Account/Account.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import useComponentVisible from "../../Hooks/useComponentVisible";
 import { IUser } from "../../Redux/Slices/userSlice";
 
 type props = {
@@ -7,31 +8,47 @@ type props = {
 };
 
 const Account = ({ user }: props) => {
+    const [clicked, setClicked] = useState(false);
+    const { ref, isComponentVisible, setIsComponentVisible } =
+        useComponentVisible();
+
     return (
         <div className="ml-auto">
             <div>
                 <img
-                    className="w-[50px] h-[50px] cursor-pointer"
+                    className="w-[50px] h-[50px] cursor-pointer p-1"
                     src={`data:${
                         user.avatar.contentType
                     };utf8,${encodeURIComponent(user.avatar.data)}`}
                     alt={user.firstName + " " + user.lastName}
+                    onClick={() => {
+                        if (!clicked) {
+                            setIsComponentVisible(true);
+                            setClicked(true);
+                        } else {
+                            setClicked(false);
+                        }
+                    }}
                 ></img>
-
-                <div className="absolute translate-x-[-75%] translate-y-[-25%] bg-white flex flex-col gap-2 shadow-sm shadow-gray-500 rounded-md p-4 text-center">
-                    <Link
-                        to=""
-                        className="hover:bg-gray-200 w-full px-3 py-1 rounded-sm"
+                {isComponentVisible && (
+                    <div
+                        className="absolute translate-x-[-75%] translate-y-[-25%] bg-white flex flex-col gap-2 shadow-sm shadow-gray-500 rounded-md p-4 text-center"
+                        ref={ref}
                     >
-                        Account
-                    </Link>
-                    <Link
-                        to=""
-                        className="hover:bg-gray-200 w-full px-3 py-1 rounded-sm"
-                    >
-                        Setting
-                    </Link>
-                </div>
+                        <Link
+                            to=""
+                            className="hover:bg-gray-200 w-full px-3 py-1 rounded-sm"
+                        >
+                            Account
+                        </Link>
+                        <Link
+                            to=""
+                            className="hover:bg-gray-200 w-full px-3 py-1 rounded-sm"
+                        >
+                            Setting
+                        </Link>
+                    </div>
+                )}
             </div>
         </div>
     );
