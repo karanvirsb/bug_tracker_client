8de6dfb3213ba4868e21c9d1cff232b08d7ade1f
karanvirsb <karanvirboparai123@gmail.com>
2022-07-29 22:35:09 -0400

    added tabs

--- src/Routes/Administration/administration.tsx
+++ src/Routes/Administration/administration.tsx
@@ -8,6 +8,7 @@ import socket from "../../API/sockets";
 import { AxiosError } from "axios";
 import Spinner from "../../Components/Spinner";
 import Pagination from "../../Components/Pagination";
+import Tab from "../../Components/Tab/Tab";
 
 type mutationTypes = {
     groupNameMutationType: {
@@ -170,31 +171,32 @@ const Administration = () => {
     }, [pageNumber]);
 
     return (
-        <section className='sections p-4'>
-            <div className='mb-4'>
-                <h1 className='text-2xl font-semibold mb-4'>Group</h1>
+        <section className="sections p-4">
+            <Tab tabs={[]} components={{}}></Tab>
+            <div className="mb-4">
+                <h1 className="text-2xl font-semibold mb-4">Group</h1>
                 <form
-                    action=''
-                    className='flex flex-col gap-4'
+                    action=""
+                    className="flex flex-col gap-4"
                     onSubmit={handleSubmit}
                 >
-                    <div className='input-container'>
-                        <label htmlFor='groupName' className='input-label'>
+                    <div className="input-container">
+                        <label htmlFor="groupName" className="input-label">
                             Group Name
                         </label>
                         <input
-                            type='text'
-                            name='groupName'
-                            id='groupName'
-                            className='modal-input mx-4'
+                            type="text"
+                            name="groupName"
+                            id="groupName"
+                            className="modal-input mx-4"
                             value={groupName}
                             onChange={handleChange}
                         />
                     </div>
-                    <div className='grid grid-cols-2 w-1/3 min-w-max gap-4 md:grid-cols-1'>
+                    <div className="grid grid-cols-2 w-1/3 min-w-max gap-4 md:grid-cols-1">
                         <button
-                            type='submit'
-                            className='btn bg-blue-400 font-semibold hover:outline-2 hover:outline-blue-400 disabled:bg-zinc-400 disabled:hover:outline-none'
+                            type="submit"
+                            className="btn bg-blue-400 font-semibold hover:outline-2 hover:outline-blue-400 disabled:bg-zinc-400 disabled:hover:outline-none"
                             disabled={disableBtn}
                         >
                             {groupNameMutation.isLoading ? (
@@ -204,8 +206,8 @@ const Administration = () => {
                             )}
                         </button>
                         <button
-                            type='button'
-                            className='btn bg-green-400 font-semibold hover:outline-2 hover:outline-green-400'
+                            type="button"
+                            className="btn bg-green-400 font-semibold hover:outline-2 hover:outline-green-400"
                             onClick={refreshInviteCode}
                         >
                             {refreshMutation.isLoading ? (
@@ -218,24 +220,24 @@ const Administration = () => {
                 </form>
             </div>
             <div>
-                <h1 className='text-xl font-semibold mb-4'>Members</h1>
-                <div className='outline-[#D4D4D4] outline-1 outline p-4 text-left rounded-md mx-4'>
-                    <table className='w-full'>
-                        <thead className='text-sm text-gray-500 font-normal'>
+                <h1 className="text-xl font-semibold mb-4">Members</h1>
+                <div className="outline-[#D4D4D4] outline-1 outline p-4 text-left rounded-md mx-4">
+                    <table className="w-full">
+                        <thead className="text-sm text-gray-500 font-normal">
                             <tr>
-                                <th scope='col' className='px-6 py-3 sm:hidden'>
+                                <th scope="col" className="px-6 py-3 sm:hidden">
                                     {/* Avatar */}
                                 </th>
-                                <th scope='col' className='px-6 py-3 md:hidden'>
+                                <th scope="col" className="px-6 py-3 md:hidden">
                                     USERNAME
                                 </th>
                                 <th
-                                    scope='col'
-                                    className='px-6 py-3 sm:text-center'
+                                    scope="col"
+                                    className="px-6 py-3 sm:text-center"
                                 >
                                     FULL NAME
                                 </th>
-                                <th scope='col' className='px-6 py-3 md:hidden'>
+                                <th scope="col" className="px-6 py-3 md:hidden">
                                     EMAIL
                                 </th>
                             </tr>
