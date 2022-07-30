67d4a3116ac749d7d4102e81184e66794573cc86
karanvirsb <karanvirboparai123@gmail.com>
2022-07-30 10:22:01 -0400

    added styling and positioned menu

--- src/Components/Account/Account.tsx
+++ src/Components/Account/Account.tsx
@@ -1,22 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { IUser } from "../../Redux/Slices/userSlice";
 
 type props = {
-  user: IUser;
+    user: IUser;
 };
 
 const Account = ({ user }: props) => {
-  return (
-    <div className="ml-auto">
-      <img
-        className="w-[50px] h-[50px] cursor-pointer"
-        src={`data:${user.avatar.contentType};utf8,${encodeURIComponent(
-          user.avatar.data
-        )}`}
-        alt={user.firstName + " " + user.lastName}
-      ></img>
-    </div>
-  );
+    return (
+        <div className="ml-auto">
+            <div>
+                <img
+                    className="w-[50px] h-[50px] cursor-pointer"
+                    src={`data:${
+                        user.avatar.contentType
+                    };utf8,${encodeURIComponent(user.avatar.data)}`}
+                    alt={user.firstName + " " + user.lastName}
+                ></img>
+
+                <div className="absolute translate-x-[-75%] translate-y-[-25%] bg-white flex flex-col gap-2 shadow-sm shadow-gray-500 rounded-md p-4 text-center">
+                    <Link
+                        to=""
+                        className="hover:bg-gray-200 w-full px-3 py-1 rounded-sm"
+                    >
+                        Account
+                    </Link>
+                    <Link
+                        to=""
+                        className="hover:bg-gray-200 w-full px-3 py-1 rounded-sm"
+                    >
+                        Setting
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Account;
