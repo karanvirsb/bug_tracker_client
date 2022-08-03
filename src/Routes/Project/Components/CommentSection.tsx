96a3c91e0f1541f99c2c8be551eec5e1bbcb10dd
mrboparai <karanvirboparai123@gmail.com>
2022-08-03 16:23:48 -0400

    handled submit of a comment

--- src/Routes/Project/Components/CommentSection.tsx
+++ src/Routes/Project/Components/CommentSection.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../Hooks/hooks";
 import axiosPrivate from "../../../Components/AxiosInterceptors";
-import { useInfiniteQuery } from "react-query";
+import { useInfiniteQuery, useMutation } from "react-query";
 import { setComments } from "../../../Redux/Slices/commentsSlice";
 import { IComment } from "../../../Redux/Slices/commentsSlice";
 import Comments from "./Comments";
+import { toast } from "react-toastify";
+import socket from "../../../API/sockets";
 
 type props = {
     ticketId: string;
@@ -17,6 +19,7 @@ type fetchCommentSectionType = {
 const CommentSection = ({ ticketId }: props) => {
     const [loadComments, setLoadComments] = useState(false);
     const [currentPage, setCurrentPage] = useState(1);
+    const [commentInput, setCommentInput] = useState("");
     const user = useAppSelector((state) => state.persistedReducer.user);
     const ticketComments = useAppSelector((state) => state.comments.comments);
     const dispatch = useAppDispatch();
@@ -43,14 +46,39 @@ const CommentSection = ({ ticketId }: props) => {
         }
     );
 
+    const postCommentMutation = useMutation(async (commentInfo: IComment) => {
+        return await axiosPrivate("/comment", {
+            method: "post",
+            data: commentInfo,
+        });
+    });
+
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+
+        const comment: IComment = {
+            userId: user.username,
+            ticketId: ticketId,
+            comment: commentInput,
+        };
+
+        postCommentMutation.mutateAsync(comment, {
+            onSuccess: () => {
+                socket.emit("invalidateQuery", {
+                    queryName: "comments" + ticketId,
+                    groupId: ticketId,
+                });
+                toast.success("Commment has been posted");
+
+                setCommentInput("");
+            },
+        });
     };
 
     useEffect(() => {
         if (commentsStatus === "success") {
             const fetchedComments: IComment[] = comments.pages;
-            dispatch(setComments((fetchedComments as any).docs));
+            dispatch(setComments((fetchedComments[0] as any).docs));
         }
     }, [commentsStatus, comments]);
 
@@ -68,6 +96,8 @@ const CommentSection = ({ ticketId }: props) => {
                     <input
                         className='border-b-[2px] border-b-gray-400 px-2 py-2 text-lg w-[75%] min-w-[250px] max-w-[1250px] focus:outline-none'
                         type='text'
+                        onChange={(e) => setCommentInput(e.target.value)}
+                        value={commentInput}
                     />
                     <button
                         className='outline outline-[1px] outline-black px-4 py-2 w-24'
@@ -79,14 +109,16 @@ const CommentSection = ({ ticketId }: props) => {
             </form>
             {commentsStatus === "success" &&
                 (ticketComments.length > 0 ? (
-                    <div className='w-full flex justify-center items-center mt-4'>
-                        <button
-                            className='btn hover:!outline-none hover:!text-blue-500'
-                            onClick={() => setLoadComments(true)}
-                        >
-                            Load Comments
-                        </button>
-                    </div>
+                    !loadComments && (
+                        <div className='w-full flex justify-center items-center mt-4'>
+                            <button
+                                className='btn hover:!outline-none hover:!text-blue-500'
+                                onClick={() => setLoadComments(true)}
+                            >
+                                Load Comments
+                            </button>
+                        </div>
+                    )
                 ) : (
                     <div className='w-full text-center text-xl font-semibold mt-4'>
                         No comments. Be the first to comment any concerns.
