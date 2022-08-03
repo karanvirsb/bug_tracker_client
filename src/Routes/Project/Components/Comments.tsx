d6099875096ace0ae6f2ac57b6e45c2a48b49fc9
mrboparai <karanvirboparai123@gmail.com>
2022-08-03 15:33:32 -0400

    created a map over comments

---
+++ src/Routes/Project/Components/Comments.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useAppSelector } from "../../../Hooks/hooks";
+import Comment from "./Comment";
+
+const Comments = () => {
+    const comments = useAppSelector((state) => state.comments.comments);
+    const users = useAppSelector((state) => state.persistedReducer.group.users);
+    return (
+        <div className='flex flex-col items-center gap-4 mt-4 w-full'>
+            {comments.map((comment) => {
+                const user = users.find(
+                    (user) => user.username === comment.userId
+                );
+                if (comment?.ticketId && user) {
+                    return <Comment comment={comment} user={user}></Comment>;
+                }
+            })}
+        </div>
+    );
+};
+
+export default Comments;
