b545300cf0d215371a85ffbf3bba727194c729c5
mrboparai <karanvirboparai123@gmail.com>
2022-08-03 16:48:31 -0400

    added replyTo functionality

--- src/Routes/Project/Components/ReplyToForm.tsx
+++ src/Routes/Project/Components/ReplyToForm.tsx
@@ -1,18 +1,46 @@
 import React, { useState } from "react";
+import { useMutation } from "react-query";
+import { toast } from "react-toastify";
+import socket from "../../../API/sockets";
+import axiosPrivate from "../../../Components/AxiosInterceptors";
 import { useAppSelector } from "../../../Hooks/hooks";
 import { IComment } from "../../../Redux/Slices/commentsSlice";
 
 type props = {
     repliedToUserId: string;
     comment: IComment;
+    setReplying: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const ReplyToForm = ({ repliedToUserId, comment }: props) => {
+const ReplyToForm = ({ repliedToUserId, comment, setReplying }: props) => {
     const [replyInput, setReplyInput] = useState(`@${repliedToUserId} `);
     const user = useAppSelector((state) => state.persistedReducer.user);
 
+    const replyMutation = useMutation(async (commentInfo: IComment) => {
+        return await axiosPrivate("/comment/reply", {
+            method: "post",
+            data: { commentId: comment?.commentId, reply: commentInfo },
+        });
+    });
+
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        const reply: IComment = {
+            userId: user.username,
+            comment: replyInput,
+        };
+
+        replyMutation.mutateAsync(reply, {
+            onSuccess: () => {
+                toast.success("Replied Successfully");
+                socket.emit("invalidateQuery", {
+                    queryName: "comments" + comment.ticketId,
+                    groupId: comment.ticketId,
+                });
+                setReplying(false);
+                setReplyInput("");
+            },
+        });
     };
     return (
         <form
