0f6379b1a7ed18778ee1fc06371b853d95abcdf7
mrboparai <karanvirboparai123@gmail.com>
2022-08-06 09:57:03 -0400

    extracted ternary operator to a variable

--- src/Components/PersistLogin/persistLogin.tsx
+++ src/Components/PersistLogin/persistLogin.tsx
@@ -1,7 +1,8 @@
-import { Outlet } from "react-router-dom";
 import React, { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
 import useRefreshToken from "../../Hooks/useRefreshToken";
 import { useAppSelector } from "../../Hooks/hooks";
+import Spinner from "../Spinner";
 
 const PersistLogin = () => {
     const [isLoading, setIsLoading] = useState(true);
@@ -30,17 +31,17 @@ const PersistLogin = () => {
         };
     }, [auth.accessToken, refresh]);
 
-    return (
-        <>
-            {!persist ? (
-                <Outlet></Outlet>
-            ) : isLoading ? (
-                <p>Loading....</p>
-            ) : (
-                <Outlet></Outlet>
-            )}
-        </>
+    const persistTernary: JSX.Element = isLoading ? (
+        <div className='fixed inset-0 flex justify-center items-center'>
+            <div className='bg-black w-20 h-20 rounded-lg flex justify-center items-center'>
+                <Spinner></Spinner>
+            </div>
+        </div>
+    ) : (
+        <Outlet></Outlet>
     );
+
+    return <>{!persist ? <Outlet></Outlet> : persistTernary}</>;
 };
 
 export default PersistLogin;
