d91433c5f5ca52fdfa1bb810624c2538225367e2
mrboparai <karanvirboparai123@gmail.com>
2022-08-05 21:53:32 -0400

    returned null

--- src/App.tsx
+++ src/App.tsx
@@ -105,7 +105,7 @@ function App() {
         });
 
         return () => {
-            window.removeEventListener("beforeunload", () => {});
+            window.removeEventListener("beforeunload", () => null);
         };
     });
 
