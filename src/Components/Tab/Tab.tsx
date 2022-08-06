0c30b101d019e693d8a64a2bdc3eab91ce127dc0
mrboparai <karanvirboparai123@gmail.com>
2022-08-06 10:02:18 -0400

    lazy loaded account

--- src/Components/Tab/Tab.tsx
+++ src/Components/Tab/Tab.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import { useAppSelector } from "../../Hooks/hooks";
-import Account from "../Account/Account";
+import Spinner from "../Spinner";
+const Account = lazy(() => import("../Account/Account"));
 
 type tabs = {
     value: string;
@@ -28,13 +29,13 @@ const Tab = ({ tabs, components }: props) => {
 
     return (
         <>
-            <nav className="flex justify-between items-center mb-2 overflow-auto mr-4">
-                <ul className="list-none flex gap-4">
+            <nav className='flex justify-between items-center mb-2 overflow-auto mr-4'>
+                <ul className='list-none flex gap-4'>
                     {tabs?.map((tab, index) => {
                         if (index === activeIndex) {
                             return (
                                 <li
-                                    className="px-4 py-1 cursor-pointer border-b-2 border-b-secondary-color"
+                                    className='px-4 py-1 cursor-pointer border-b-2 border-b-secondary-color'
                                     onClick={() => setTab(tab?.value, index)}
                                     key={tab.value}
                                 >
@@ -44,7 +45,7 @@ const Tab = ({ tabs, components }: props) => {
                         }
                         return (
                             <li
-                                className="px-4 py-1 cursor-pointer"
+                                className='px-4 py-1 cursor-pointer'
                                 onClick={() => setTab(tab?.value, index)}
                                 key={tab?.value}
                             >
@@ -53,9 +54,11 @@ const Tab = ({ tabs, components }: props) => {
                         );
                     })}
                 </ul>
-                <Account user={user}></Account>
+                <Suspense fallback={<Spinner></Spinner>}>
+                    <Account user={user}></Account>
+                </Suspense>
             </nav>
-            <section className="md:mr-1 md:ml-[-50px] p-1">
+            <section className='md:mr-1 md:ml-[-50px] p-1'>
                 {mappedComponents.get(tabName)}
             </section>
         </>
