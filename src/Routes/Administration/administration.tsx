785a8017017ce596aa824a9f5731afe67f0e4dc0
mrboparai <karanvirboparai123@gmail.com>
2022-08-06 10:59:55 -0400

    added comments

--- src/Routes/Administration/administration.tsx
+++ src/Routes/Administration/administration.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useMemo, useState } from "react";
-import Members from "./Components/Members";
+import LazyLoad from "react-lazyload";
 import { useAppSelector } from "../../Hooks/hooks";
 import { useMutation } from "react-query";
-import axiosPrivate from "../../Components/AxiosInterceptors";
 import { toast } from "react-toastify";
 import socket from "../../API/sockets";
 import { AxiosError } from "axios";
+import axiosPrivate from "../../Components/AxiosInterceptors";
+import Members from "./Components/Members";
 import Spinner from "../../Components/Spinner";
 import Pagination from "../../Components/Pagination";
 import Tab from "../../Components/Tab/Tab";
@@ -35,13 +36,17 @@ const Administration = () => {
         () => Math.floor(group.users.length / totalPerPage),
         [group.users]
     );
+    // checking if there are more pages
     const hasMore = useMemo(() => {
         return pageNumber === totalPage;
     }, [pageNumber, totalPage]);
+
+    // memoizing users
     const groupUsers = useMemo(() => {
         return group.users.slice(usersPageSetting.start, usersPageSetting.end);
     }, [usersPageSetting]);
 
+    // if group name needs to be changed
     const groupNameMutation = useMutation(
         async ({ id, groupName }: mutationTypes["groupNameMutationType"]) => {
             const resp = await axiosPrivate("/group/rename", {
@@ -53,6 +58,7 @@ const Administration = () => {
         }
     );
 
+    // mutation for refreshing the invite code
     const refreshMutation = useMutation(
         async ({ id, groupName }: mutationTypes["refreshMutation"]) => {
             const resp = await axiosPrivate("/group/refresh", {
@@ -64,6 +70,7 @@ const Administration = () => {
         }
     );
 
+    // handling the change of input
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.value !== group.groupName) {
             setDisableBtn(false);
@@ -76,6 +83,7 @@ const Administration = () => {
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
+        // making sure the name meets the requirements
         if (groupName.length < 4 || groupName.length > 50) {
             toast.error("Group Name should be between 4 and 50 characters");
             setGroupName(group.groupName);
@@ -171,32 +179,34 @@ const Administration = () => {
     }, [pageNumber]);
 
     return (
-        <section className="sections">
-            <Tab tabs={[]} components={{}}></Tab>
-            <div className="mb-4 px-4">
-                <h1 className="text-2xl font-semibold mb-4">Group</h1>
+        <section className='sections'>
+            <LazyLoad>
+                <Tab tabs={[]} components={{}}></Tab>
+            </LazyLoad>
+            <div className='mb-4 px-4'>
+                <h1 className='text-2xl font-semibold mb-4'>Group</h1>
                 <form
-                    action=""
-                    className="flex flex-col gap-4"
+                    action=''
+                    className='flex flex-col gap-4'
                     onSubmit={handleSubmit}
                 >
-                    <div className="input-container">
-                        <label htmlFor="groupName" className="input-label">
+                    <div className='input-container'>
+                        <label htmlFor='groupName' className='input-label'>
                             Group Name
                         </label>
                         <input
-                            type="text"
-                            name="groupName"
-                            id="groupName"
-                            className="modal-input mx-4"
+                            type='text'
+                            name='groupName'
+                            id='groupName'
+                            className='modal-input mx-4'
                             value={groupName}
                             onChange={handleChange}
                         />
                     </div>
-                    <div className="grid grid-cols-2 w-1/3 min-w-max gap-4 md:grid-cols-1">
+                    <div className='grid grid-cols-2 w-1/3 min-w-max gap-4 md:grid-cols-1'>
                         <button
-                            type="submit"
-                            className="btn bg-blue-400 font-semibold hover:outline-2 hover:outline-blue-400 disabled:bg-zinc-400 disabled:hover:outline-none"
+                            type='submit'
+                            className='btn bg-blue-400 font-semibold hover:outline-2 hover:outline-blue-400 disabled:bg-zinc-400 disabled:hover:outline-none'
                             disabled={disableBtn}
                         >
                             {groupNameMutation.isLoading ? (
@@ -206,8 +216,8 @@ const Administration = () => {
                             )}
                         </button>
                         <button
-                            type="button"
-                            className="btn bg-green-400 font-semibold hover:outline-2 hover:outline-green-400"
+                            type='button'
+                            className='btn bg-green-400 font-semibold hover:outline-2 hover:outline-green-400'
                             onClick={refreshInviteCode}
                         >
                             {refreshMutation.isLoading ? (
@@ -219,39 +229,43 @@ const Administration = () => {
                     </div>
                 </form>
             </div>
-            <div className="p-4">
-                <h1 className="text-xl font-semibold mb-4">Members</h1>
-                <div className="outline-[#D4D4D4] outline-1 outline p-4 text-left rounded-md mx-4">
-                    <table className="w-full">
-                        <thead className="text-sm text-gray-500 font-normal">
+            <div className='p-4'>
+                <h1 className='text-xl font-semibold mb-4'>Members</h1>
+                <div className='outline-[#D4D4D4] outline-1 outline p-4 text-left rounded-md mx-4'>
+                    <table className='w-full'>
+                        <thead className='text-sm text-gray-500 font-normal'>
                             <tr>
-                                <th scope="col" className="px-6 py-3 sm:hidden">
+                                <th scope='col' className='px-6 py-3 sm:hidden'>
                                     {/* Avatar */}
                                 </th>
-                                <th scope="col" className="px-6 py-3 md:hidden">
+                                <th scope='col' className='px-6 py-3 md:hidden'>
                                     USERNAME
                                 </th>
                                 <th
-                                    scope="col"
-                                    className="px-6 py-3 sm:text-center"
+                                    scope='col'
+                                    className='px-6 py-3 sm:text-center'
                                 >
                                     FULL NAME
                                 </th>
-                                <th scope="col" className="px-6 py-3 md:hidden">
+                                <th scope='col' className='px-6 py-3 md:hidden'>
                                     EMAIL
                                 </th>
                             </tr>
                         </thead>
                         <tbody>
-                            <Members users={groupUsers}></Members>
+                            <LazyLoad>
+                                <Members users={groupUsers}></Members>
+                            </LazyLoad>
                         </tbody>
                     </table>
-                    <Pagination
-                        pageNumber={pageNumber}
-                        setPageNumber={setPageNumber}
-                        totalPage={totalPage}
-                        hasMore={hasMore}
-                    ></Pagination>
+                    <LazyLoad>
+                        <Pagination
+                            pageNumber={pageNumber}
+                            setPageNumber={setPageNumber}
+                            totalPage={totalPage}
+                            hasMore={hasMore}
+                        ></Pagination>
+                    </LazyLoad>
                 </div>
             </div>
         </section>
