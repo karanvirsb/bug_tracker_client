493ad4de0aa81b7eaf8741da5380808ebf096747
mrboparai <karanvirboparai123@gmail.com>
2022-08-07 13:01:45 -0400

    changed serve port

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
     server: {
         port: 3000,
     },
+    preview: {
+        port: 3000,
+    },
     // This changes the out put dir from dist to build
     build: {
         outDir: "build",
