440f278f16dabc122b607424f8955d3d1fb988bd
mrboparai <karanvirboparai123@gmail.com>
2022-08-08 19:03:40 -0400

    editted suspense

--- src/Routes/Dashboard/Components/EditProjectModal.tsx
+++ src/Routes/Dashboard/Components/EditProjectModal.tsx
@@ -157,7 +157,7 @@ const EditProjectModal = (props: { projectId: string }): JSX.Element => {
             >
                 <Suspense
                     fallback={
-                        <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
+                        <div className='bg-white w-full rounded-lg flex justify-center items-center'>
                             <Spinner></Spinner>
                         </div>
                     }
@@ -170,19 +170,19 @@ const EditProjectModal = (props: { projectId: string }): JSX.Element => {
                         refs={usersSelected}
                         type={"edit"}
                     ></ProjectModal>
+                    <div className='flex justify-center items-center gap-2 md:flex-col md:items-stretch md:px-20 sm:px-0'>
+                        <button type='submit' className='btn bg-blue-500 !px-8'>
+                            Submit
+                        </button>
+                        <button
+                            type='button'
+                            onClick={closeModal}
+                            className='btn bg-red-500 !px-6'
+                        >
+                            Cancel
+                        </button>
+                    </div>
                 </Suspense>
-                <div className='flex justify-center items-center gap-2 md:flex-col md:items-stretch md:px-20 sm:px-0'>
-                    <button type='submit' className='btn bg-blue-500 !px-8'>
-                        Submit
-                    </button>
-                    <button
-                        type='button'
-                        onClick={closeModal}
-                        className='btn bg-red-500 !px-6'
-                    >
-                        Cancel
-                    </button>
-                </div>
             </form>
         </motion.div>
     );
