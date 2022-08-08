88dd8c153da43d18f78303ad264da551d9417fa6
mrboparai <karanvirboparai123@gmail.com>
2022-08-07 22:07:35 -0400

    created a separate root element to load in

---
+++ src/Root.tsx
@@ -0,0 +1,35 @@
+import React, { ComponentType } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { persistor, store } from "./Redux/Store";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { AxiosInterceptor } from "./Components/AxiosInterceptors";
+
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+);
+
+const render = (App: ComponentType) => {
+    root.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <Provider store={store}>
+                    <PersistGate persistor={persistor}>
+                        <AxiosInterceptor>
+                            <QueryClientProvider client={queryClient}>
+                                <App />
+                            </QueryClientProvider>
+                        </AxiosInterceptor>
+                    </PersistGate>
+                </Provider>
+            </BrowserRouter>
+        </React.StrictMode>
+    );
+};
+
+export default render;
