9f76e90d5bdfb2880f3a25b942ee6081851f6d12
mrboparai <karanvirboparai123@gmail.com>
2022-08-10 12:55:41 -0400

    fixed header color and padding

--- src/Routes/Tickets/Components/UserTicketInfoModal.tsx
+++ src/Routes/Tickets/Components/UserTicketInfoModal.tsx
@@ -35,99 +35,97 @@ const UserTicketInfoModal = ({ selectedId, setSelectedId }: props) => {
     return (
         <Backdrop>
             <motion.div
-                className='bg-white flex flex-col gap-4 fixed bottom-0 left-0 right-0 p-4 max-w-[1920px] w-full top-[25%] overflow-auto rounded-md'
+                className='bg-white flex flex-col gap-4 fixed bottom-0 left-0 right-0 max-w-[1920px] w-full top-[25%] overflow-auto rounded-md'
                 variants={ticketModalConstraints}
                 initial='hidden'
                 animate='visible'
                 exit='exit'
             >
-                <div className='flex justify-between items-center'>
-                    <div className='flex lg:flex-col gap-4 items-center'>
-                        <div className='flex gap-4 sm:flex-col m-md:items-center sm:w-full'>
-                            <h1 className='text-2xl'>{foundTicket?.title}</h1>
-                            <span className='sm:hidden'>&#8212;</span>
-                            <span className='text-gray-600 m-md:pl-2'>
-                                {new Date(
-                                    foundTicket?.dateCreated ?? ""
-                                ).toDateString()}
+                <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='h-9 w-9 absolute top-[10px] right-[10px] z-20 fill-gray-600 cursor-pointer hover:fill-red-400 hover:text-black'
+                    viewBox='0 0 20 20'
+                    fill='currentColor'
+                    onClick={() => setSelectedId(null)}
+                >
+                    <path
+                        fillRule='evenodd'
+                        d='M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z'
+                        clipRule='evenodd'
+                    />
+                </svg>
+
+                <div className='bg-gray-100 flex lg:flex-col gap-4 w-full p-4'>
+                    <div className='flex gap-4 sm:flex-col m-md:items-center sm:w-full'>
+                        <h1 className='text-2xl'>{foundTicket?.title}</h1>
+                        <span className='sm:hidden'>&#8212;</span>
+                        <span className='text-gray-600 m-md:pl-2'>
+                            {new Date(
+                                foundTicket?.dateCreated ?? ""
+                            ).toDateString()}
+                        </span>
+                    </div>
+                    <span className='hidden m-lg:inline-block'>&#8212;</span>
+                    <div className='flex gap-4 sm:flex-col m-md:items-center sm:w-full '>
+                        <div className='flex gap-4 items-center'>
+                            <span
+                                className={`${
+                                    ticketTypeColor[
+                                        foundTicket?.ticketType ?? "Bug"
+                                    ]
+                                } text-center rounded-xl px-4`}
+                            >
+                                {foundTicket?.ticketType}
+                            </span>{" "}
+                            <span
+                                className={`${
+                                    ticketStatusColor[
+                                        foundTicket?.ticketStatus ?? "Open"
+                                    ]
+                                } text-center rounded-xl px-4`}
+                            >
+                                {foundTicket?.ticketStatus}
+                            </span>{" "}
+                            <span
+                                className={`${
+                                    ticketSeverityColor[
+                                        foundTicket?.ticketSeverity ?? "None"
+                                    ]
+                                } text-center px-4`}
+                            >
+                                {foundTicket?.ticketSeverity}
                             </span>
                         </div>
-                        <span className='hidden m-lg:inline-block'>
-                            &#8212;
-                        </span>
-                        <div className='flex gap-4 sm:flex-col m-md:items-center sm:w-full '>
-                            <div className='flex gap-4 items-center'>
-                                <span
-                                    className={`${
-                                        ticketTypeColor[
-                                            foundTicket?.ticketType ?? "Bug"
-                                        ]
-                                    } text-center rounded-xl px-4`}
-                                >
-                                    {foundTicket?.ticketType}
-                                </span>{" "}
-                                <span
-                                    className={`${
-                                        ticketStatusColor[
-                                            foundTicket?.ticketStatus ?? "Open"
-                                        ]
-                                    } text-center rounded-xl px-4`}
-                                >
-                                    {foundTicket?.ticketStatus}
-                                </span>{" "}
-                                <span
-                                    className={`${
-                                        ticketSeverityColor[
-                                            foundTicket?.ticketSeverity ??
-                                                "None"
-                                        ]
-                                    } text-center px-4`}
-                                >
-                                    {foundTicket?.ticketSeverity}
-                                </span>
-                            </div>
-                            <span className='sm:hidden'>&#8212;</span>
-                            <div>
-                                By:{" "}
-                                {foundUser?.firstName +
-                                    " " +
-                                    foundUser?.lastName}
-                            </div>
+                        <span className='sm:hidden'>&#8212;</span>
+                        <div>
+                            By:{" "}
+                            {foundUser?.firstName + " " + foundUser?.lastName}
                         </div>
                     </div>
-                    <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        className='h-9 w-9 fill-gray-600 cursor-pointer hover:fill-red-400 hover:text-black'
-                        viewBox='0 0 20 20'
-                        fill='currentColor'
-                        onClick={() => setSelectedId(null)}
-                    >
-                        <path
-                            fillRule='evenodd'
-                            d='M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z'
-                            clipRule='evenodd'
-                        />
-                    </svg>
                 </div>
-                <div className='grid grid-cols-2 gap-4 sm:grid-cols-1'>
-                    <div>
-                        <h2 className='text-gray-500 text-lg'>Description:</h2>
-                        <p className='max-w-[100ch] w-full text-lg max-h-[150px] overflow-auto pl-4 outline outline-gray-200 outline-1 rounded-md p-4'>
-                            {foundTicket?.description}
-                        </p>
+                <div className='p-4'>
+                    <div className='grid grid-cols-2 gap-4 sm:grid-cols-1 mb-6'>
+                        <div>
+                            <h2 className='text-gray-500 text-lg'>
+                                Description:
+                            </h2>
+                            <p className='max-w-[100ch] w-full text-lg max-h-[150px] overflow-auto pl-4 outline outline-gray-200 outline-1 rounded-md p-4'>
+                                {foundTicket?.description}
+                            </p>
+                        </div>
+                        <div>
+                            <h2 className='text-gray-500 text-lg'>Users:</h2>
+                            <UserElements
+                                usersArr={foundTicket?.assignedDev ?? []}
+                            ></UserElements>
+                        </div>
                     </div>
                     <div>
-                        <h2 className='text-gray-500 text-lg'>Users:</h2>
-                        <UserElements
-                            usersArr={foundTicket?.assignedDev ?? []}
-                        ></UserElements>
+                        {/* TODO go to ticket */}
+                        <button className='btn bg-blue-400 hover:outline hover:outline-2 hover:outline-blue-400 mt-4 '>
+                            Go To Ticket
+                        </button>
                     </div>
-                </div>
-                <div>
-                    {/* TODO go to ticket */}
-                    <button className='btn bg-blue-400 hover:outline hover:outline-2 hover:outline-blue-400 mt-4 '>
-                        Go To Ticket
-                    </button>
                 </div>
             </motion.div>
         </Backdrop>
