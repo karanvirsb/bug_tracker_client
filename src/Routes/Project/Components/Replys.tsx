a6091b42dff4e36a91d8a7bc1f3e9f20d1708c91
mrboparai <karanvirboparai123@gmail.com>
2022-08-14 10:50:26 -0400

    updated with infintie query

--- src/Routes/Project/Components/Replys.tsx
+++ src/Routes/Project/Components/Replys.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useMemo } from "react";
-import { useQuery } from "react-query";
+import React, { useEffect, useMemo, useState } from "react";
+import { useInfiniteQuery, useQuery } from "react-query";
 import axiosPrivate from "../../../Components/AxiosInterceptors";
 import Spinner from "../../../Components/Spinner";
 import { useAppDispatch, useAppSelector } from "../../../Hooks/hooks";
+import useInvalidateQuery from "../../../Hooks/useInvalidateQuery";
 import { IComment } from "../../../Redux/Slices/commentsSlice";
 import { setReplys } from "../../../Redux/Slices/replysSlice";
 import Comment from "./Comment";
@@ -11,35 +12,83 @@ type props = {
     replyIds: string[];
     commentId: string;
 };
+
+const TOTAL_REPLYS_VISIBLE = 5;
+
 // TODO paginate infintie
 const Replys = ({ replyIds, commentId }: props) => {
+    const totalPages = useMemo(() => {
+        if (replyIds) return Math.floor(replyIds.length / TOTAL_REPLYS_VISIBLE);
+    }, [replyIds, replyIds?.length]);
+
     const commentReplys = useAppSelector((state) => state.replys.replys);
     const users = useAppSelector((state) => state.persistedReducer.group.users);
     const dispatch = useAppDispatch();
 
-    const fetchReplys = async (replyIds: string[]) => {
+    const replyIdsSplice = (currentPage: number) => {
+        return [...replyIds].splice(
+            currentPage * TOTAL_REPLYS_VISIBLE,
+            (currentPage + 1) * TOTAL_REPLYS_VISIBLE
+        );
+    };
+
+    // const fetchReplys = async (replyIds: string[]) => {
+    //     const resp = await axiosPrivate("/comment/reply/comments", {
+    //         method: "post",
+    //         data: { replyIdArr: replyIds },
+    //     });
+    //     return resp.data;
+    // };
+
+    const fetchReplys = async ({ pageParam = 0 }) => {
+        const replyIdsArr = replyIdsSplice(pageParam); // calling method to get the array
         const resp = await axiosPrivate("/comment/reply/comments", {
             method: "post",
-            data: { replyIdArr: replyIds },
+            data: { replyIdArr: replyIdsArr },
         });
-        return resp.data;
+        if (totalPages !== undefined && totalPages >= 0) {
+            console.log(pageParam + 1 < totalPages);
+            return {
+                response: resp.data,
+                hasNextPage: pageParam + 1 < totalPages,
+                nextPage: pageParam < totalPages ? pageParam + 1 : null,
+            };
+        }
     };
+
+    // const {
+    //     data: replys,
+    //     status: replyStatus,
+    //     refetch: refetchReplys,
+    // } = useQuery("replies-" + commentId, () => fetchReplys(replyIds));
 
     const {
         data: replys,
+        isFetchingNextPage,
+        hasNextPage,
         status: replyStatus,
-        refetch: refetchReplys,
-    } = useQuery("replies-" + commentId, () => fetchReplys(replyIds));
+        fetchNextPage,
+        refetch,
+    } = useInfiniteQuery(["replies-" + commentId], fetchReplys, {
+        getNextPageParam: (lastPage) => {
+            return lastPage?.nextPage ?? undefined;
+        },
+    });
 
     useEffect(() => {
         if (replyStatus === "success") {
-            dispatch(setReplys({ id: commentId, comments: replys }));
+            const totalReplys: IComment[] = [];
+            const replysLength = replys.pages.length;
+
+            for (let i = 0; i < replysLength; i++) {
+                totalReplys.push(...replys?.pages[i]?.response);
+            }
+            dispatch(setReplys({ id: commentId, comments: totalReplys }));
         }
     }, [replys, replyStatus]);
 
     useEffect(() => {
-        refetchReplys();
-        console.log(replyIds);
+        refetch();
     }, [replyIds]);
 
     return (
@@ -67,6 +116,12 @@ const Replys = ({ replyIds, commentId }: props) => {
                         );
                     }
                 })}
+            {hasNextPage && (
+                <button onClick={() => fetchNextPage()}>
+                    {isFetchingNextPage && "Fetching replies"}
+                    {hasNextPage && "Load More Replies"}
+                </button>
+            )}
         </>
     );
 };
