96722355b1237e32a280f5bbb075b5beac460527
mrboparai <karanvirboparai123@gmail.com>
2022-08-15 09:49:03 -0400

    react refresh is deprecated

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import reactRefresh from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 import manifest from "./public/manifest.json";
