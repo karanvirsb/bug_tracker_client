b1b1ffdae6219f13f80b86037fdb54145523464b
mrboparai <karanvirboparai123@gmail.com>
2022-08-18 14:59:01 -0400

    added the nav bar hover

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -121,25 +121,44 @@ const Navbar = () => {
                 showNavigation
                     ? " md:w-[50vw] sm:w-full md:block md:-z-1"
                     : " md:bg-transparent md:text-black md:w-max"
-            } ${modal && "md:hidden"}`}
+            }`}
+            // ${modal && "md:hidden"}
         >
-            <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className={`h-8 w-8 md:${
-                    showNavigation ? "hidden" : "block"
-                } m-lg:hidden`}
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-                strokeWidth={2}
-                onClick={openModal}
-            >
-                <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M4 6h16M4 12h16M4 18h16'
-                />
-            </svg>
+            <div className='m-md:hidden fixed bottom-2 right-2 bg-white outline outline-1 outline-gray-200 shadow-xl rounded-full p-4'>
+                {showNavigation ? (
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        className={`h-8 w-8 text-black`}
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='currentColor'
+                        strokeWidth={2}
+                        onClick={closeModal}
+                    >
+                        <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            d='M6 18L18 6M6 6l12 12'
+                        />
+                    </svg>
+                ) : (
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        className={`h-8 w-8`}
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='currentColor'
+                        strokeWidth={2}
+                        onClick={openModal}
+                    >
+                        <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            d='M4 6h16M4 12h16M4 18h16'
+                        />
+                    </svg>
+                )}
+            </div>
             <nav
                 className={`flex flex-col justify-between min-h-full ${
                     showNavigation ? "" : "md:hidden"
@@ -177,23 +196,6 @@ const Navbar = () => {
                                 </div>
                             )}
                         </h1>
-                        <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            className={`h-8 w-8 m-md:hidden cursor-pointer ${
-                                showNavigation ? "inline-block" : "hidden"
-                            }`}
-                            fill='none'
-                            viewBox='0 0 24 24'
-                            stroke='currentColor'
-                            strokeWidth={2}
-                            onClick={closeModal}
-                        >
-                            <path
-                                strokeLinecap='round'
-                                strokeLinejoin='round'
-                                d='M6 18L18 6M6 6l12 12'
-                            />
-                        </svg>
                     </div>
                     <div className=' flex flex-col gap-4 py-4 m-md:text-center'>
                         <NavLink to='/dashboard' className={classNameFunc}>
