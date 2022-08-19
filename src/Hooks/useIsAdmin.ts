c4cf304151c0be6fc197cb002585db1f6b2095be
mrboparai <karanvirboparai123@gmail.com>
2022-08-19 14:19:57 -0400

    added a useEffect

--- src/Hooks/useIsAdmin.ts
+++ src/Hooks/useIsAdmin.ts
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
 import { useAppSelector } from "./hooks";
 
 // Checking to see if a user is an admin or an editor
 const useIsAdmin = () => {
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [isEditor, setIsEditor] = useState(false);
     const user = useAppSelector((state) => state.persistedReducer.user);
 
-    return { isAdmin: user.isAdmin, isEditor: user.isEditor };
+    useEffect(() => {
+        setIsAdmin(user.isAdmin);
+    }, [user.isAdmin]);
+
+    useEffect(() => {
+        setIsEditor(user.isEditor);
+    }, [user.isEditor]);
+
+    return { isAdmin: isAdmin, isEditor: isEditor };
 };
 
 export default useIsAdmin;
