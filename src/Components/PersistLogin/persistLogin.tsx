2441ebaa3dba03442c3f80265321af166eae10b4
mrboparai <karanvirboparai123@gmail.com>
2022-08-19 14:19:49 -0400

    added a useEffect to update user info

--- src/Components/PersistLogin/persistLogin.tsx
+++ src/Components/PersistLogin/persistLogin.tsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import useRefreshToken from "../../Hooks/useRefreshToken";
-import { useAppSelector } from "../../Hooks/hooks";
+import { useAppDispatch, useAppSelector } from "../../Hooks/hooks";
 import Spinner from "../Spinner";
+import axiosPrivate from "../AxiosInterceptors";
+import { setUser } from "../../Redux/Slices/userSlice";
 
 const PersistLogin = () => {
     const [isLoading, setIsLoading] = useState(true);
     const refresh = useRefreshToken();
     const auth = useAppSelector((state) => state.persistedReducer.auth);
     const persist = useAppSelector((state) => state.persist.persist);
+    const user = useAppSelector((state) => state.persistedReducer.user);
+    const dispatch = useAppDispatch();
 
     useEffect(() => {
         let isMounted = true;
@@ -30,6 +34,38 @@ const PersistLogin = () => {
             isMounted = false;
         };
     }, [auth.accessToken, refresh]);
+
+    useEffect(() => {
+        const getUserInfo = async () => {
+            const resp = await axiosPrivate("/user/id", {
+                method: "post",
+                data: {
+                    filter: "username",
+                    filterValue: auth.username,
+                },
+            });
+            if (resp) {
+                const user = resp.data;
+                const roles: string[] = Object.values(user.roles);
+                console.log(user);
+                dispatch(
+                    setUser({
+                        avatar: user.avatar,
+                        username: user.username,
+                        email: user.email,
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        isAdmin: roles?.includes("1990") ?? false,
+                        isEditor: roles?.includes("1991") ?? false,
+                    })
+                );
+            }
+        };
+        if (auth.username && persist && !user.username) {
+            console.log("here");
+            getUserInfo();
+        }
+    }, [auth.username, persist]);
 
     const persistTernary: JSX.Element = isLoading ? (
         <div className='fixed inset-0 flex justify-center items-center'>
