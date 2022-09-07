2340a3d4b0124c140daf73c485c3bd8aaa6b9ddd
mrboparai <karanvirboparai123@gmail.com>
2022-09-07 10:49:12 -0400

    added type and text input

--- src/Components/ErrorFallback/index.tsx
+++ src/Components/ErrorFallback/index.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-const ErrorFallback = (props: { error: any; resetErrorBoundary: any }) => {
+
+type props = {
+    error: any;
+    resetErrorBoundary?: any;
+    text: string;
+};
+const ErrorFallback = ({ error, resetErrorBoundary, text }: props) => {
     return (
         <>
             <tr className='w-full text-center text-lg '>
                 <td colSpan={1000}>
-                    <p className='my-3 text-xl'>Could Not Fetch Projects!</p>
-                    <button
-                        className='btn bg-gray-300 mb-5'
-                        onClick={props.resetErrorBoundary}
-                    >
-                        try again
-                    </button>
+                    <p className='my-3 text-xl'>{text}</p>
+                    {resetErrorBoundary && (
+                        <button
+                            className='btn bg-gray-300 mb-5'
+                            onClick={resetErrorBoundary}
+                        >
+                            try again
+                        </button>
+                    )}
                 </td>
             </tr>
         </>
