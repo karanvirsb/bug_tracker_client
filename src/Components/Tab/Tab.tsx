5967a5f8a4d6ad3917de8c651169e055496e7a5e
mrboparai <karanvirboparai123@gmail.com>
2022-09-08 10:56:27 -0400

    added error checking for account component

--- src/Components/Tab/Tab.tsx
+++ src/Components/Tab/Tab.tsx
@@ -1,4 +1,5 @@
 import React, { useState, lazy, Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { useAppSelector } from "../../Hooks/hooks";
 import Spinner from "../Spinner";
 const Account = lazy(() => import("../Account/Account"));
@@ -54,9 +55,31 @@ const Tab = ({ tabs, components }: props) => {
                         );
                     })}
                 </ul>
-                <Suspense fallback={<Spinner></Spinner>}>
-                    <Account user={user}></Account>
-                </Suspense>
+                <ErrorBoundary
+                    fallback={
+                        <div className='text-red-500 flex flex-col justify-center items-center'>
+                            <svg
+                                xmlns='http://www.w3.org/2000/svg'
+                                fill='none'
+                                viewBox='0 0 24 24'
+                                strokeWidth={1.5}
+                                stroke='currentColor'
+                                className='w-6 h-6'
+                            >
+                                <path
+                                    strokeLinecap='round'
+                                    strokeLinejoin='round'
+                                    d='M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z'
+                                />
+                            </svg>{" "}
+                            Account Error
+                        </div>
+                    }
+                >
+                    <Suspense fallback={<Spinner></Spinner>}>
+                        <Account user={user}></Account>
+                    </Suspense>
+                </ErrorBoundary>
             </nav>
             <section className='md:mr-1 p-1'>
                 {mappedComponents.get(tabName)}
