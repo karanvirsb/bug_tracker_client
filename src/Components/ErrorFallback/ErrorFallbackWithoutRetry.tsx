f08c4350c3247cba50077f19286d80adaa404f55
mrboparai <karanvirboparai123@gmail.com>
2022-09-08 12:14:03 -0400

    made it accept text

--- src/Components/ErrorFallback/ErrorFallbackWithoutRetry.tsx
+++ src/Components/ErrorFallback/ErrorFallbackWithoutRetry.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 
 type props = {
-    error: any;
-    resetErrorBoundary?: any;
+    text: string;
 };
 
-const ErrorFallbackWithoutRetry = ({ error, resetErrorBoundary }: props) => {
+const ErrorFallbackWithoutRetry = ({ text }: props) => {
     return (
         <>
-            <p className='text-red-500 flex gap-4 items-center my-3 text-xl w-full text-center'>
+            <p className='text-red-500 flex gap-4 justify-center items-center my-3 text-xl w-full text-center'>
                 <svg
                     xmlns='http://www.w3.org/2000/svg'
                     fill='none'
@@ -23,7 +22,7 @@ const ErrorFallbackWithoutRetry = ({ error, resetErrorBoundary }: props) => {
                         d='M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z'
                     />
                 </svg>
-                Error: Could not load
+                {text}
             </p>
         </>
     );
