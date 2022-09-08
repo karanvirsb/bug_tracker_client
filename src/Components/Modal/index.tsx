5e3765c362fd173b8cc8bdf614acc2271fc402f1
mrboparai <karanvirboparai123@gmail.com>
2022-09-08 10:19:10 -0400

    imported error boundary and error modal

--- src/Components/Modal/index.tsx
+++ src/Components/Modal/index.tsx
@@ -1,4 +1,6 @@
 import React, { lazy, Suspense } from "react";
+import { motion } from "framer-motion";
+import { ErrorBoundary } from "react-error-boundary";
 const AnimatePresence = lazy(async () => {
     const { AnimatePresence } = await import("framer-motion");
     return { default: AnimatePresence };
@@ -7,6 +9,7 @@ const AnimatePresence = lazy(async () => {
 import { useAppSelector } from "../../Hooks/hooks";
 import Backdrop from "../Backdrop";
 import Spinner from "../Spinner";
+import ErrorModal from "../ErrorModal";
 const DeleteMemberModal = lazy(
     () => import("../../Routes/Administration/Components/DeleteMemberModal")
 );
@@ -38,123 +41,147 @@ const RemovedFromGroupModal = lazy(
 const Modal = () => {
     const classname = "!w-30 !h-30";
     const modal = useAppSelector((state) => state.modal);
+
     return (
         <AnimatePresence exitBeforeEnter={true} initial={false}>
             {modal.open && (
                 <Backdrop>
-                    {modal.type === "createProject" && (
-                        <Suspense
-                            fallback={
-                                <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
-                                    <Spinner classname={classname}></Spinner>
-                                </div>
-                            }
-                        >
-                            <AddProjectModal></AddProjectModal>
-                        </Suspense>
-                    )}
-                    {modal.type === "updateProject" && modal.options && (
-                        <Suspense
-                            fallback={
-                                <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
-                                    <Spinner classname={classname}></Spinner>
-                                </div>
-                            }
-                        >
-                            <EditProjectModal
-                                projectId={modal.options?.projectId ?? ""}
-                            ></EditProjectModal>
-                        </Suspense>
-                    )}
-                    {modal.type === "deleteProject" && modal.options.projectId && (
-                        <Suspense
-                            fallback={
-                                <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
-                                    <Spinner classname={classname}></Spinner>
-                                </div>
-                            }
-                        >
-                            <DeleteProjectModal
-                                projectId={modal.options?.projectId ?? ""}
-                            ></DeleteProjectModal>
-                        </Suspense>
-                    )}
-                    {modal.type === "createTicket" && (
-                        <Suspense
-                            fallback={
-                                <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
-                                    <Spinner classname={classname}></Spinner>
-                                </div>
-                            }
-                        >
-                            <AddTicketModal></AddTicketModal>
-                        </Suspense>
-                    )}
-                    {modal.type === "updateTicket" && (
-                        <Suspense
-                            fallback={
-                                <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
-                                    <Spinner classname={classname}></Spinner>
-                                </div>
-                            }
-                        >
-                            <EditTicketModal
-                                ticketId={modal.options?.ticketId ?? ""}
-                            ></EditTicketModal>
-                        </Suspense>
-                    )}
-                    {modal.type === "deleteTicket" && (
-                        <Suspense
-                            fallback={
-                                <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
-                                    <Spinner classname={classname}></Spinner>
-                                </div>
-                            }
-                        >
-                            <DeleteTicketModal
-                                ticketId={modal.options?.ticketId ?? ""}
-                            ></DeleteTicketModal>
-                        </Suspense>
-                    )}
-                    {modal.type === "editMember" && (
-                        <Suspense
-                            fallback={
-                                <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
-                                    <Spinner classname={classname}></Spinner>
-                                </div>
-                            }
-                        >
-                            <EditMemberModal
-                                username={modal.options.username ?? ""}
-                            ></EditMemberModal>
-                        </Suspense>
-                    )}
+                    <ErrorBoundary FallbackComponent={ErrorModal}>
+                        {modal.type === "createProject" && (
+                            <Suspense
+                                fallback={
+                                    <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
+                                        <Spinner
+                                            classname={classname}
+                                        ></Spinner>
+                                    </div>
+                                }
+                            >
+                                <AddProjectModal></AddProjectModal>
+                            </Suspense>
+                        )}
+                        {modal.type === "updateProject" && modal.options && (
+                            <Suspense
+                                fallback={
+                                    <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
+                                        <Spinner
+                                            classname={classname}
+                                        ></Spinner>
+                                    </div>
+                                }
+                            >
+                                <EditProjectModal
+                                    projectId={modal.options?.projectId ?? ""}
+                                ></EditProjectModal>
+                            </Suspense>
+                        )}
+                        {modal.type === "deleteProject" &&
+                            modal.options.projectId && (
+                                <Suspense
+                                    fallback={
+                                        <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
+                                            <Spinner
+                                                classname={classname}
+                                            ></Spinner>
+                                        </div>
+                                    }
+                                >
+                                    <DeleteProjectModal
+                                        projectId={
+                                            modal.options?.projectId ?? ""
+                                        }
+                                    ></DeleteProjectModal>
+                                </Suspense>
+                            )}
+                        {modal.type === "createTicket" && (
+                            <Suspense
+                                fallback={
+                                    <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
+                                        <Spinner
+                                            classname={classname}
+                                        ></Spinner>
+                                    </div>
+                                }
+                            >
+                                <AddTicketModal></AddTicketModal>
+                            </Suspense>
+                        )}
+                        {modal.type === "updateTicket" && (
+                            <Suspense
+                                fallback={
+                                    <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
+                                        <Spinner
+                                            classname={classname}
+                                        ></Spinner>
+                                    </div>
+                                }
+                            >
+                                <EditTicketModal
+                                    ticketId={modal.options?.ticketId ?? ""}
+                                ></EditTicketModal>
+                            </Suspense>
+                        )}
+                        {modal.type === "deleteTicket" && (
+                            <Suspense
+                                fallback={
+                                    <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
+                                        <Spinner
+                                            classname={classname}
+                                        ></Spinner>
+                                    </div>
+                                }
+                            >
+                                <DeleteTicketModal
+                                    ticketId={modal.options?.ticketId ?? ""}
+                                ></DeleteTicketModal>
+                            </Suspense>
+                        )}
+                        {modal.type === "editMember" && (
+                            <Suspense
+                                fallback={
+                                    <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
+                                        <Spinner
+                                            classname={classname}
+                                        ></Spinner>
+                                    </div>
+                                }
+                            >
+                                <EditMemberModal
+                                    username={modal.options.username ?? ""}
+                                ></EditMemberModal>
+                            </Suspense>
+                        )}
 
-                    {modal.type === "removeMember" && (
-                        <Suspense
-                            fallback={
-                                <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
-                                    <Spinner classname={classname}></Spinner>
-                                </div>
-                            }
-                        >
-                            <DeleteMemberModal
-                                username={modal.options.username ?? ""}
-                            ></DeleteMemberModal>
-                        </Suspense>
-                    )}
+                        {modal.type === "removeMember" && (
+                            <Suspense
+                                fallback={
+                                    <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
+                                        <Spinner
+                                            classname={classname}
+                                        ></Spinner>
+                                    </div>
+                                }
+                            >
+                                <DeleteMemberModal
+                                    username={modal.options.username ?? ""}
+                                ></DeleteMemberModal>
+                            </Suspense>
+                        )}
 
-                    {modal.type === "removedUserModal" && (
-                        <Suspense
-                            fallback={
-                                <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
-                                    <Spinner classname={classname}></Spinner>
-                                </div>
-                            }
-                        >
-                            <RemovedFromGroupModal></RemovedFromGroupModal>
-                        </Suspense>
-                    )}
+                        {modal.type === "removedUserModal" && (
+                            <Suspense
+                                fallback={
+                                    <div className='bg-white w-20 h-20 rounded-lg flex justify-center items-center'>
+                                        <Spinner
+                                            classname={classname}
+                                        ></Spinner>
+                                    </div>
+                                }
+                            >
+                                <RemovedFromGroupModal></RemovedFromGroupModal>
+                            </Suspense>
+                        )}
+                    </ErrorBoundary>
                 </Backdrop>
             )}
         </AnimatePresence>
