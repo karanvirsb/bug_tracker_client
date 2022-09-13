d3bfaaa18e4be353157c868160c7fd230f3e6f3c
mrboparai <karanvirboparai123@gmail.com>
2022-09-13 10:31:21 -0400

    made more functions

--- src/Hooks/useLogout.ts
+++ src/Hooks/useLogout.ts
@@ -5,48 +5,68 @@ import socket from "../API/sockets";
 import { setGroup } from "../Redux/Slices/groupSlice";
 import { setUser } from "../Redux/Slices/userSlice";
 import { changePersist } from "../Auth/persistSlice";
+import { Dispatch, AnyAction } from "redux";
 
 const useLogout = () => {
     const dispatch = useDispatch();
     const logout = async () => {
-        // reseting group slice
-        dispatch(
-            setGroup({
-                groupId: "",
-                groupInviteCode: "",
-                groupName: "",
-                users: [],
-            })
-        );
-        // reseting user slice
-        dispatch(
-            setUser({
-                avatar: { data: "", contentType: "" },
-                username: "",
-                email: "",
-                firstName: "",
-                lastName: "",
-                isAdmin: false,
-                isEditor: false,
-            })
-        );
-
-        socket.emit("leavingPage");
-        socket.disconnect();
-
-        // resetting authentication
-        dispatch(
-            setAuth({ username: "", accessToken: "", group_id: "", roles: [] })
-        );
-
-        dispatch(changePersist(false));
         try {
             await axios.delete("/logout", { withCredentials: true });
+            // reseting group slice
+            resetGroup(dispatch);
+            // reseting user slice
+            resetUser(dispatch);
+
+            sendSocketInfo();
+
+            // resetting authentication
+            resetAuth(dispatch);
+
+            resetPersist(dispatch);
         } catch (err) {
-            console.log("error");
+            // console.log("error");
         }
     };
     return logout;
 };
 
 export default useLogout;
+function resetPersist(dispatch: Dispatch<AnyAction>) {
+    dispatch(changePersist(false));
+}
+
+function resetAuth(dispatch: Dispatch<AnyAction>) {
+    dispatch(
+        setAuth({ username: "", accessToken: "", group_id: "", roles: [] })
+    );
+}
+
+function sendSocketInfo() {
+    socket.emit("leavingPage");
+    socket.disconnect();
+}
+
+function resetUser(dispatch: Dispatch<AnyAction>) {
+    dispatch(
+        setUser({
+            avatar: { data: "", contentType: "" },
+            username: "",
+            email: "",
+            firstName: "",
+            lastName: "",
+            isAdmin: false,
+            isEditor: false,
+        })
+    );
+}
+
+function resetGroup(dispatch: Dispatch<AnyAction>) {
+    dispatch(
+        setGroup({
+            groupId: "",
+            groupInviteCode: "",
+            groupName: "",
+            users: [],
+        })
+    );
+}
