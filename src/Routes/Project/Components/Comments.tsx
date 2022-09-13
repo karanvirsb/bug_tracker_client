e899d266cf54bc7a622a55011181255f353f6861
mrboparai <karanvirboparai123@gmail.com>
2022-09-13 14:09:26 -0400

    added disable comment and removed unused import

--- src/Routes/Project/Components/Comments.tsx
+++ src/Routes/Project/Components/Comments.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import { useAppSelector } from "../../../Hooks/hooks";
 import Comment from "./Comment";
 
@@ -9,27 +9,30 @@ const Comments = () => {
     let page: number = 0;
     return (
         <div className='flex flex-col items-center gap-4 mt-4 w-full'>
-            {comments.map((comment) => {
-                if (count === 5) {
-                    page += 1;
-                    count = 0;
-                }
-                const user = users.find(
-                    (user) => user.username === comment.userId
-                );
-                count += 1;
-                if (comment?.ticketId && user) {
-                    return (
-                        <Comment
-                            key={comment.commentId}
-                            comment={comment}
-                            user={user}
-                            classname='flex flex-row gap-4 py-4 w-[100%]'
-                            page={page}
-                        ></Comment>
+            {
+                // eslint-disable-next-line array-callback-return
+                comments.map((comment) => {
+                    if (count === 5) {
+                        page += 1;
+                        count = 0;
+                    }
+                    const user = users.find(
+                        (user) => user.username === comment.userId
                     );
-                }
-            })}
+                    count += 1;
+                    if (comment?.ticketId && user) {
+                        return (
+                            <Comment
+                                key={comment.commentId}
+                                comment={comment}
+                                user={user}
+                                classname='flex flex-row gap-4 py-4 w-[100%]'
+                                page={page}
+                            ></Comment>
+                        );
+                    }
+                })
+            }
         </div>
     );
 };
